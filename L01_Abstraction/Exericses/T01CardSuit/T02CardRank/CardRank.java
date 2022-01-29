29a8c3899b0dda65a04def774ef29cc4183d8a3a
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-01-29 10:10:58 +0200

    Add files via upload

---
+++ L01_Abstraction/Exericses/T01CardSuit/T02CardRank/CardRank.java
@@ -0,0 +1,17 @@
+package M04_JavaOOP.L01_Abstraction.Exercises.T02CardRank;
+
+public enum CardRank {
+    ACE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING
+}

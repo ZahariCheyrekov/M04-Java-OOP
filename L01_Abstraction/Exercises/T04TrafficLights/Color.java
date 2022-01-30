195c730784567b03fcb3a8ab0be27348837ac18d
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-01-30 11:02:37 +0200

    Add files via upload

---
+++ L01_Abstraction/Exercises/T04TrafficLights/Color.java
@@ -0,0 +1,7 @@
+package M04_JavaOOP.L01_Abstraction.Exercises.T04TrafficLights;
+
+public enum Color {
+    RED,
+    YELLOW,
+    GREEN;
+}
---
+++ L01_Abstraction/Exercises/T04TrafficLights/TrafficLight.java
@@ -0,0 +1,30 @@
+package M04_JavaOOP.L01_Abstraction.Exercises.T04TrafficLights;
+
+public class TrafficLight {
+    private Color color;
+
+    public TrafficLight(Color color) {
+        this.color = color;
+    }
+
+    public void changeColor() {
+        switch (this.color) {
+            case RED:
+                this.color = Color.GREEN;
+                break;
+
+            case GREEN:
+                this.color = Color.YELLOW;
+                break;
+
+            case YELLOW:
+                this.color = Color.RED;
+                break;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return this.color.toString();
+    }
+}

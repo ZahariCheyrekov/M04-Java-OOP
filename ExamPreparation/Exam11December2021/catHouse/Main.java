60445491a5f092f1863a2ab32c9ed801ef77b026
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-02-14 16:50:22 +0200

    Add files via upload

---
+++ ExamPreparation/Exam11December2021/catHouse/Main.java
@@ -0,0 +1,11 @@
+package M04_JavaOOP.ExamPreparation.Exam11December2021.catHouse;
+
+import M04_JavaOOP.ExamPreparation.Exam11December2021.catHouse.core.Engine;
+import M04_JavaOOP.ExamPreparation.Exam11December2021.catHouse.core.EngineImpl;
+
+public class Main {
+    public static void main(String[] args) {
+        Engine engine = new EngineImpl();
+        engine.run();
+    }
+}

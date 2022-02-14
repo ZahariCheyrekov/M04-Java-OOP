af5f458b851026d06d9df1fa124d040ed1f2bd29
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-02-14 22:03:00 +0200

    Update FarmvilleTests.java

--- ExamPreparation/Exam22August2021/glacialExpedition/farmville-test/FarmvilleTests.java
+++ ExamPreparation/Exam22August2021/glacialExpedition/farmville-test/FarmvilleTests.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 
 public class FarmvilleTests {
 
-    //Everything works fine.
     @Test
     public void constructorShouldCreateFarm() {
         String name = "Cow farm";
@@ -16,42 +15,34 @@ public class FarmvilleTests {
         Assert.assertEquals(capacity, farm.getCapacity());
     }
 
-    //name is null -> exception
     @Test(expected = NullPointerException.class)
     public void constructorShouldThrowExceptionBecauseNameIsNull() {
         new Farm(null, 5);
     }
 
-    //name is only white characters -> exception
     @Test(expected = NullPointerException.class)
     public void constructorShouldThrowExceptionBecauseNameIsEmpty() {
         new Farm("     ", 5);
     }
 
-    //capacity < 0 -> exception
     @Test(expected = IllegalArgumentException.class)
     public void constructorShouldThrowExceptionBecauseCapacityIsNegative() {
         new Farm("Cow Farm", -1);
     }
 
-    //1. if size == capacity -> Exception
     @Test(expected = IllegalArgumentException.class)
     public void addMethodShouldThrowBecauseFarmIsFull() {
         Farm farm = new Farm("Cow Farm", 0);
         farm.add(new Animal("Cow", 5));
-
     }
 
-    //2. if animal exists -> Exception
     @Test(expected = IllegalArgumentException.class)
     public void addMethodShouldThrowBecauseAnimalExists() {
         Farm farm = new Farm("Cow Farm", 5);
         farm.add(new Animal("Cow", 5));
         farm.add(new Animal("Cow", 12));
-
     }
 
-    //3. animal is added
     @Test
     public void addMethodShouldAddAnimal() {
         Farm farm = new Farm("Cow Farm", 5);
@@ -60,8 +51,6 @@ public class FarmvilleTests {
         Assert.assertEquals(1, farm.getCount());
     }
 
-
-    //1. animal is removed
     @Test
     public void removeMethodShouldRemoveAnimal() {
         Farm farm = new Farm("Cow Farm", 5);
@@ -70,11 +59,8 @@ public class FarmvilleTests {
         Assert.assertEquals(1, farm.getCount());
         Assert.assertTrue(farm.remove("Pig"));
         Assert.assertEquals(0, farm.getCount());
-
-
     }
 
-    //2. no animal is removed
     @Test
     public void removeMethodShouldNotRemoveAnimal() {
         Farm farm = new Farm("Cow Farm", 5);

eb210af0efb3fc2d26dcc089c34d11b7e44326c6
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-08 09:57:28 +0200

    Update Main.java

--- L01_Abstraction/Exercises/T01CardSuit/Main.java
+++ L01_Abstraction/Exercises/T01CardSuit/Main.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+        
         System.out.println("Card Suits:");
 
         Arrays.stream(CardSuits.values())

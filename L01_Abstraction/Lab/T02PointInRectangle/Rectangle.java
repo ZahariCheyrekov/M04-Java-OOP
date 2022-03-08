e22a458c33d25edce86e98752c9b7f486ce61a69
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-08 08:54:04 +0200

    Update Rectangle.java

--- L01_Abstraction/Lab/T02PointInRectangle/Rectangle.java
+++ L01_Abstraction/Lab/T02PointInRectangle/Rectangle.java
@@ -1,8 +1,8 @@
-package M04_JavaOOP.L01_Abstraction.Lab.T02PointInRectangle;
+package pointInRectangle;
 
 public class Rectangle {
-    private final Point pointA;
-    private final Point pointB;
+    private Point pointA;
+    private Point pointB;
 
     public Rectangle(Point pointA, Point pointB) {
         this.pointA = pointA;

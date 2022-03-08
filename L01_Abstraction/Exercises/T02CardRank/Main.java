60d1c4895a91209f8db46e5d7f304a6016b29341
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-08 09:57:47 +0200

    Update Main.java

--- L01_Abstraction/Exercises/T02CardRank/Main.java
+++ L01_Abstraction/Exercises/T02CardRank/Main.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Card Ranks:");
 
         Arrays.stream(CardRank.values())

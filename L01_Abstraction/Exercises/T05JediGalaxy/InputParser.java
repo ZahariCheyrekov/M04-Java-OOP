8fc988bb1cc950ccb10e964adb01a698b4c275ad
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-08 10:04:02 +0200

    Update InputParser.java

--- L01_Abstraction/Exercises/T05JediGalaxy/InputParser.java
+++ L01_Abstraction/Exercises/T05JediGalaxy/InputParser.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class InputParser {
     public static int[] parseIntegerArray(String input) {
+       
         return Arrays.stream(input.split("[\\s]+"))
                 .mapToInt(Integer::parseInt)
                 .toArray();

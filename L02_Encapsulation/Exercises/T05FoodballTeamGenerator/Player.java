c3b456061944aa3eba87b6182257038646fbe9fd
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-09 12:14:20 +0200

    Update Player.java

--- L02_Encapsulation/Exercises/T05FoodballTeamGenerator/Player.java
+++ L02_Encapsulation/Exercises/T05FoodballTeamGenerator/Player.java
@@ -1,5 +1,12 @@
 package M04_JavaOOP.L02_Encapsulation.Exercises.T05FoodballTeamGenerator;
 
 public class Player {
+    private String name;
+    private int endurance;
+    private int sprint;
+    private int dribble;
+    private int passing;
+    private int shooting;
+    
     
 }

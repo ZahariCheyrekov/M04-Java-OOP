eb7d7ba50ee14fd10c25b04ea7cfa2ddc52b141d
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-11 10:37:19 +0200

    Update Car.java

--- L05_Polymorphism/Exercises/T01Vehicles/Car.java
+++ L05_Polymorphism/Exercises/T01Vehicles/Car.java
@@ -1,6 +1,7 @@
 package M04_JavaOOP.L05_Polymorphism.Exercises.T01Vehicles;
 
 public class Car extends VehicleImpl {
+
     private static final double AC_ADDITIONAL_CONSUMPTION = 0.9;
 
     public Car(double fuelQuantity, double fuelConsumption) {

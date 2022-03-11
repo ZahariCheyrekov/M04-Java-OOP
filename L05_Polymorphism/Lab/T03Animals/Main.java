46b33b6a7787c88d1759eef1fa21a10f17096aed
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-11 10:34:35 +0200

    Update Main.java

--- L05_Polymorphism/Lab/T03Animals/Main.java
+++ L05_Polymorphism/Lab/T03Animals/Main.java
@@ -2,6 +2,7 @@ package M04_JavaOOP.L05_Polymorphism.Lab.T03Animals;
 
 public class Main {
     public static void main(String[] args) {
+     
         Animal cat = new Cat("Oscar", "Whiskas");
         Animal dog = new Dog("Rocky", "Meat");
         System.out.println(cat.explainSelf());

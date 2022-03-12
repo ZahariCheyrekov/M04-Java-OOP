652eb69983b5f9c4b664e8f2dfe2b4995c69b635
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-12 11:59:34 +0200

    Update Frog.java

--- L03_Inheritance/Exercises/T06Animals/animals/Frog.java
+++ L03_Inheritance/Exercises/T06Animals/animals/Frog.java
@@ -1,6 +1,7 @@
 package M04_JavaOOP.L03_Inheritance.Exercises.T06Animals.animals;
 
 public class Frog extends Animal {
+
     public Frog(String name, int age, String gender) {
         super(name, age, gender);
     }

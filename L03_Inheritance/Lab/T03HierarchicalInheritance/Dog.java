21d9e18397820d0abba5786aa840c64eaa3b3633
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-12 11:59:04 +0200

    Update Dog.java

--- L03_Inheritance/Lab/T03HierarchicalInheritance/Dog.java
+++ L03_Inheritance/Lab/T03HierarchicalInheritance/Dog.java
@@ -2,6 +2,7 @@ package M04_JavaOOP.L03_Inheritance.Lab.T03HierarchicalInheritance;
 
 public class Dog extends Animal {
     public void bark() {
+       
         System.out.println("barking...");
     }
 }

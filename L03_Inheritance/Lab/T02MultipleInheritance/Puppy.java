b6fef52107e166973c0c3bec217e79b10c2dd1e6
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-12 11:58:24 +0200

    Update Puppy.java

--- L03_Inheritance/Lab/T02MultipleInheritance/Puppy.java
+++ L03_Inheritance/Lab/T02MultipleInheritance/Puppy.java
@@ -2,6 +2,7 @@ package M04_JavaOOP.L03_Inheritance.Lab.T02MultipleInheritance;
 
 public class Puppy extends Dog {
     public void weep() {
+      
         System.out.println("weeping...");
     }
 }

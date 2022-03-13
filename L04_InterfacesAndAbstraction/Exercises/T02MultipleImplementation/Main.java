a00538b744f1a41545abbbdbcab4de6ac9dedf14
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-13 15:56:35 +0200

    Update Main.java

--- L04_InterfacesAndAbstraction/Exercises/T02MultipleImplementation/Main.java
+++ L04_InterfacesAndAbstraction/Exercises/T02MultipleImplementation/Main.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+      
         Class[] citizenInterfaces = Citizen.class.getInterfaces();
 
         if (Arrays.asList(citizenInterfaces).contains(Birthable.class)

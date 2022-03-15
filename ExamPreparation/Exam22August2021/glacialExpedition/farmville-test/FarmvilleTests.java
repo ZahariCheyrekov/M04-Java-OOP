9d0df8756e1abb144aa81a157927e9ff63837020
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-15 18:31:51 +0200

    Update FarmvilleTests.java

--- ExamPreparation/Exam22August2021/glacialExpedition/farmville-test/FarmvilleTests.java
+++ ExamPreparation/Exam22August2021/glacialExpedition/farmville-test/FarmvilleTests.java
@@ -5,13 +5,6 @@ import org.junit.Test;
 
 public class FarmvilleTests {
 
-    @Test(expected = IllegalArgumentException.class)
-    public void addMethodShouldThrowBecauseAnimalExists() {
-        Farm farm = new Farm("Cow Farm", 5);
-        farm.add(new Animal("Cow", 5));
-        farm.add(new Animal("Cow", 12));
-    }
-
     @Test
     public void addMethodShouldAddAnimal() {
         Farm farm = new Farm("Cow Farm", 5);

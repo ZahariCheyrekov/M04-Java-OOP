dafc20c714d44b775524077427a700240572e406
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-15 18:32:13 +0200

    Update FarmvilleTests.java

--- ExamPreparation/Exam22August2021/glacialExpedition/farmville-test/FarmvilleTests.java
+++ ExamPreparation/Exam22August2021/glacialExpedition/farmville-test/FarmvilleTests.java
@@ -5,13 +5,4 @@ import org.junit.Test;
 
 public class FarmvilleTests {
 
-    @Test
-    public void removeMethodShouldNotRemoveAnimal() {
-        Farm farm = new Farm("Cow Farm", 5);
-        Assert.assertEquals(0, farm.getCount());
-        farm.add(new Animal("Pig", 120));
-        Assert.assertEquals(1, farm.getCount());
-        Assert.assertFalse(farm.remove("Dog"));
-        Assert.assertEquals(1, farm.getCount());
-    }
 }

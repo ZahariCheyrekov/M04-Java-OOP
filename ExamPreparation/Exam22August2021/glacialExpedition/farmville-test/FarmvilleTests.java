4bf790f03d25f0860c89ddb4f0d31f5ab718cca8
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-15 18:34:33 +0200

    Update FarmvilleTests.java

--- ExamPreparation/Exam22August2021/glacialExpedition/farmville-test/FarmvilleTests.java
+++ ExamPreparation/Exam22August2021/glacialExpedition/farmville-test/FarmvilleTests.java
@@ -5,4 +5,14 @@ import org.junit.Test;
 
 public class FarmvilleTests {
 
+   private static Farm farm;
+   private static Animal animal;
+
+    @Before
+    public void setUp() {
+        farm = new Farm("Old Duck", 2);
+        animal = new Animal("Lion", 100);
+        farm.add(animal);
+    }
+  
 }

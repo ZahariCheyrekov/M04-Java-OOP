7251929fa86cba12497dd74519736369462380c8
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-16 13:16:10 +0200

    Update ControllerImpl.java

--- ExamPreparation/Exam15August2021/restaurant/core/ControllerImpl.java
+++ ExamPreparation/Exam15August2021/restaurant/core/ControllerImpl.java
@@ -1,21 +1,158 @@
-package M04_JavaOOP.ExamPreparation.Exam15August2021.restaurant.core;
+package restaurant.core;
 
-import M04_JavaOOP.ExamPreparation.Exam15August2021.restaurant.core.interfaces.Controller;
-import M04_JavaOOP.ExamPreparation.Exam15August2021.restaurant.entities.drinks.Fresh;
-import M04_JavaOOP.ExamPreparation.Exam15August2021.restaurant.entities.drinks.Smoothie;
-import M04_JavaOOP.ExamPreparation.Exam15August2021.restaurant.entities.healthyFoods.Salad;
-import M04_JavaOOP.ExamPreparation.Exam15August2021.restaurant.entities.healthyFoods.VeganBiscuits;
-import M04_JavaOOP.ExamPreparation.Exam15August2021.restaurant.entities.healthyFoods.interfaces.HealthyFood;
-import M04_JavaOOP.ExamPreparation.Exam15August2021.restaurant.entities.drinks.interfaces.Beverages;
-import M04_JavaOOP.ExamPreparation.Exam15August2021.restaurant.entities.tables.InGarden;
-import M04_JavaOOP.ExamPreparation.Exam15August2021.restaurant.entities.tables.Indoors;
-import M04_JavaOOP.ExamPreparation.Exam15August2021.restaurant.entities.tables.interfaces.Table;
-import M04_JavaOOP.ExamPreparation.Exam15August2021.restaurant.repositories.interfaces.BeverageRepository;
-import M04_JavaOOP.ExamPreparation.Exam15August2021.restaurant.repositories.interfaces.HealthFoodRepository;
-import M04_JavaOOP.ExamPreparation.Exam15August2021.restaurant.repositories.interfaces.TableRepository;
+import restaurant.core.interfaces.Controller;
+import restaurant.entities.drinks.Fresh;
+import restaurant.entities.drinks.Smoothie;
+import restaurant.entities.healthyFoods.Salad;
+import restaurant.entities.healthyFoods.VeganBiscuits;
+import restaurant.entities.healthyFoods.interfaces.HealthyFood;
+import restaurant.entities.drinks.interfaces.Beverages;
+import restaurant.entities.tables.InGarden;
+import restaurant.entities.tables.Indoors;
+import restaurant.entities.tables.interfaces.Table;
+import restaurant.repositories.interfaces.*;
 
 import java.util.Collection;
 
-import static M04_JavaOOP.ExamPreparation.Exam15August2021.restaurant.common.ExceptionMessages.*;
-import static M04_JavaOOP.ExamPreparation.Exam15August2021.restaurant.common.OutputMessages.*;
+import static restaurant.common.ExceptionMessages.*;
+import static restaurant.common.OutputMessages.*;
 
+public class ControllerImpl implements Controller {
+
+    private final HealthFoodRepository<HealthyFood> healthFoodRepository;
+    private final BeverageRepository<Beverages> beverageRepository;
+    private final TableRepository<Table> tableRepository;
+    private double totalMoney;
+
+    public ControllerImpl(HealthFoodRepository<HealthyFood> healthFoodRepository,
+                          BeverageRepository<Beverages> beverageRepository,
+                          TableRepository<Table> tableRepository) {
+
+        this.healthFoodRepository = healthFoodRepository;
+        this.beverageRepository = beverageRepository;
+        this.tableRepository = tableRepository;
+    }
+
+    @Override
+    public String addHealthyFood(String type, double price, String name) {
+
+        HealthyFood food = type.equals("Salad")
+                ? new Salad(name, price)
+                : new VeganBiscuits(name, price);
+
+        HealthyFood previouslyAdded = this.healthFoodRepository.foodByName(name);
+
+        if (previouslyAdded != null) {
+            throw new IllegalArgumentException(String.format(FOOD_EXIST, name));
+        }
+
+        this.healthFoodRepository.add(food);
+        return String.format(FOOD_ADDED, name);
+    }
+
+    @Override
+    public String addBeverage(String type, int counter, String brand, String name) {
+
+        Beverages beverage = type.equals("Fresh")
+                ? new Fresh(name, counter, brand)
+                : new Smoothie(name, counter, brand);
+
+        Beverages previouslyAdded = this.beverageRepository.beverageByName(name, brand);
+
+        if (previouslyAdded != null) {
+            throw new IllegalArgumentException(String.format(BEVERAGE_EXIST, name));
+        }
+
+        this.beverageRepository.add(beverage);
+        return String.format(BEVERAGE_ADDED, type, brand);
+    }
+
+    @Override
+    public String addTable(String type, int tableNumber, int capacity) {
+
+        Table table = type.equals("Indoors")
+                ? new Indoors(tableNumber, capacity)
+                : new InGarden(tableNumber, capacity);
+
+        Table previouslyAdded = this.tableRepository.byNumber(tableNumber);
+
+        if (previouslyAdded != null) {
+            throw new IllegalArgumentException(String.format(TABLE_IS_ALREADY_ADDED, tableNumber));
+        }
+
+        this.tableRepository.add(table);
+        return String.format(TABLE_ADDED, tableNumber);
+    }
+
+    @Override
+    public String reserve(int numberOfPeople) {
+
+        Collection<Table> tables = this.tableRepository.getAllEntities();
+
+        Table suitableTable = tables
+                .stream()
+                .filter(table -> !table.isReservedTable() && table.getSize() >= numberOfPeople)
+                .findFirst()
+                .orElse(null);
+
+        if (suitableTable == null) {
+            return String.format(RESERVATION_NOT_POSSIBLE, numberOfPeople);
+        }
+
+        suitableTable.reserve(numberOfPeople);
+        return String.format(TABLE_RESERVED, suitableTable.getTableNumber(), numberOfPeople);
+    }
+
+    @Override
+    public String orderHealthyFood(int tableNumber, String healthyFoodName) {
+        Table table = this.tableRepository.byNumber(tableNumber);
+
+        if (table == null) {
+            return String.format(WRONG_TABLE_NUMBER, tableNumber);
+        }
+
+        HealthyFood healthyFood = this.healthFoodRepository.foodByName(healthyFoodName);
+
+        if (healthyFood == null) {
+            return String.format(NONE_EXISTENT_FOOD, healthyFoodName);
+        }
+
+        table.orderHealthy(healthyFood);
+        return String.format(FOOD_ORDER_SUCCESSFUL, healthyFoodName, tableNumber);
+    }
+
+    @Override
+    public String orderBeverage(int tableNumber, String name, String brand) {
+        Table table = this.tableRepository.byNumber(tableNumber);
+
+        if (table == null) {
+            return String.format(WRONG_TABLE_NUMBER, tableNumber);
+        }
+
+        Beverages beverage = this.beverageRepository.beverageByName(name, brand);
+
+        if (beverage == null) {
+            return String.format(NON_EXISTENT_DRINK, name, brand);
+        }
+
+        table.orderBeverages(beverage);
+        return String.format(BEVERAGE_ORDER_SUCCESSFUL, name, tableNumber);
+    }
+
+    @Override
+    public String closedBill(int tableNumber) {
+        Table table = this.tableRepository.byNumber(tableNumber);
+
+        double bill = table.bill();
+        table.clear();
+
+        totalMoney += bill;
+
+        return String.format(BILL, tableNumber, bill);
+    }
+
+    @Override
+    public String totalMoney() {
+        return String.format(TOTAL_MONEY, totalMoney);
+    }
+}

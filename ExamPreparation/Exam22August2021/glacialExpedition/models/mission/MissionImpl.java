cf1b3a6cad35a8aea77efa4bd5ea721f4f8d18c2
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-16 13:12:54 +0200

    Update MissionImpl.java

--- ExamPreparation/Exam22August2021/glacialExpedition/models/mission/MissionImpl.java
+++ ExamPreparation/Exam22August2021/glacialExpedition/models/mission/MissionImpl.java
@@ -9,14 +9,13 @@ public class MissionImpl implements Mission {
 
     @Override
     public void explore(State state, Collection<Explorer> explorers) {
-        //Итерираме изследователите
-        //итерираме експонатите на конкретната локация
+
         Collection<String> stateExhibits = state.getExhibits();
+        
         for (Explorer explorer : explorers) {
-            //1 изследовател
+
             while(explorer.canSearch() && stateExhibits.iterator().hasNext()){
-                //Изследователя има енергия
-                //Има експонати за изследване
+
                 explorer.search();
                 String currentExhibit = stateExhibits.iterator().next();
                 explorer.getSuitcase().getExhibits().add(currentExhibit);

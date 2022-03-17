b25965e5abf73c7bcec7a0c3bcf473b223f40014
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-17 14:26:41 +0200

    Update ControllerImpl.java

--- ExamPreparation/Exam18April2021/spaceStation/core/ControllerImpl.java
+++ ExamPreparation/Exam18April2021/spaceStation/core/ControllerImpl.java
@@ -1,2 +1,130 @@
-package M04_JavaOOP.ExamPreparation.Exam18April2021.spaceStation.core;
+package spaceStation.core;
 
+import spaceStation.models.astronauts.Astronaut;
+import spaceStation.models.astronauts.Biologist;
+import spaceStation.models.astronauts.Geodesist;
+import spaceStation.models.astronauts.Meteorologist;
+import spaceStation.models.mission.Mission;
+import spaceStation.models.mission.MissionImpl;
+import spaceStation.models.planets.Planet;
+import spaceStation.models.planets.PlanetImpl;
+import spaceStation.repositories.AstronautRepository;
+import spaceStation.repositories.PlanetRepository;
+import spaceStation.repositories.Repository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static spaceStation.common.ExceptionMessages.*;
+import static spaceStation.common.ConstantMessages.*;
+
+public class ControllerImpl implements Controller {
+
+    private Repository<Astronaut> astronautRepository;
+    private Repository<Planet> planetRepository;
+    private int exploredPlanets;
+
+    public ControllerImpl() {
+        this.astronautRepository = new AstronautRepository();
+        this.planetRepository = new PlanetRepository();
+    }
+
+    @Override
+    public String addAstronaut(String type, String astronautName) {
+
+        Astronaut astronaut;
+
+        switch (type) {
+            case "Biologist":
+                astronaut = new Biologist(astronautName);
+                break;
+
+            case "Geodesist":
+                astronaut = new Geodesist(astronautName);
+                break;
+
+            case "Meteorologist":
+                astronaut = new Meteorologist(astronautName);
+                break;
+
+            default:
+                throw new IllegalArgumentException(ASTRONAUT_INVALID_TYPE);
+        }
+
+        this.astronautRepository.add(astronaut);
+
+        return String.format(ASTRONAUT_ADDED, type, astronautName);
+    }
+
+    @Override
+    public String addPlanet(String planetName, String... items) {
+
+        Planet planet = new PlanetImpl(planetName);
+
+        for (String item : items) {
+            planet.getItems().add(item);
+        }
+
+        this.planetRepository.add(planet);
+
+        return String.format(PLANET_ADDED, planetName);
+    }
+
+    @Override
+    public String retireAstronaut(String astronautName) {
+
+        Astronaut astronaut = this.astronautRepository.findByName(astronautName);
+
+        if (astronaut == null) {
+            throw new IllegalArgumentException(String.format(ASTRONAUT_DOES_NOT_EXIST, astronautName));
+        }
+
+        this.astronautRepository.remove(astronaut);
+
+        return String.format(ASTRONAUT_RETIRED, astronautName);
+    }
+
+    @Override
+    public String explorePlanet(String planetName) {
+
+        List<Astronaut> suitableAstronauts = this.astronautRepository
+                .getModels()
+                .stream()
+                .filter(astronaut -> astronaut.getOxygen() > 60)
+                .collect(Collectors.toList());
+
+        if (suitableAstronauts.isEmpty()) {
+            throw new IllegalArgumentException(PLANET_ASTRONAUTS_DOES_NOT_EXISTS);
+        }
+
+        Planet planet = this.planetRepository.findByName(planetName);
+
+        Mission mission = new MissionImpl();
+        mission.explore(planet, suitableAstronauts);
+        exploredPlanets++;
+
+        int deadAstronauts = mission.getDeadAstronauts();
+
+        return String.format(PLANET_EXPLORED, planetName, deadAstronauts);
+    }
+
+    @Override
+    public String report() {
+
+        StringBuilder reportInfo = new StringBuilder();
+
+        reportInfo.append(String.format(REPORT_PLANET_EXPLORED, exploredPlanets))
+                .append(System.lineSeparator());
+
+        reportInfo.append(REPORT_ASTRONAUT_INFO)
+                .append(System.lineSeparator());
+
+        this.astronautRepository
+                .getModels()
+                .forEach(astronaut -> reportInfo
+                        .append(astronaut.toString())
+                        .append(System.lineSeparator()));
+
+        return reportInfo.toString().trim();
+    }
+}

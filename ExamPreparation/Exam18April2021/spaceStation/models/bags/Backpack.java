e90e006539beee6f34e256912c1dc3d0a9f0a52d
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-17 14:29:19 +0200

    Update Backpack.java

--- ExamPreparation/Exam18April2021/spaceStation/models/bags/Backpack.java
+++ ExamPreparation/Exam18April2021/spaceStation/models/bags/Backpack.java
@@ -1,9 +1,10 @@
-package M04_JavaOOP.ExamPreparation.Exam18April2021.spaceStation.models.bags;
+package spaceStation.models.bags;
 
 import java.util.ArrayList;
 import java.util.Collection;
 
 public class Backpack implements Bag {
+
     private Collection<String> items;
 
     public Backpack() {

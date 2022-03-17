94a71ee893867f1997f312d845993d8e3bc6590a
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-17 14:29:53 +0200

    Update Meteorologist.java

--- ExamPreparation/Exam18April2021/spaceStation/models/astronauts/Meteorologist.java
+++ ExamPreparation/Exam18April2021/spaceStation/models/astronauts/Meteorologist.java
@@ -1,6 +1,7 @@
-package M04_JavaOOP.ExamPreparation.Exam18April2021.spaceStation.models.astronauts;
+package spaceStation.models.astronauts;
 
 public class Meteorologist extends BaseAstronaut {
+
     private static final double OXYGEN = 90;
 
     public Meteorologist(String name) {

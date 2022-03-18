e5818f64ec6c5ff58f1dc4ae34f0cb052d320304
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-18 12:00:17 +0200

    Update BaseFish.java

--- ExamPreparation/Exam10April2021/aquarium/entities/fish/BaseFish.java
+++ ExamPreparation/Exam10April2021/aquarium/entities/fish/BaseFish.java
@@ -1,8 +1,9 @@
-package M04_JavaOOP.ExamPreparation.Exam10April2021.aquarium.entities.fish;
+package aquarium.entities.fish;
 
-import static M04_JavaOOP.ExamPreparation.Exam10April2021.aquarium.common.ExceptionMessages.*;
+import static aquarium.common.ExceptionMessages.*;
 
 public abstract class BaseFish implements Fish {
+
     private String name;
     private String species;
     private int size;
@@ -14,31 +15,35 @@ public abstract class BaseFish implements Fish {
         this.setPrice(price);
     }
 
-    public void setSpecies(String species) {
+    @Override
+    public void setName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new NullPointerException(FISH_NAME_NULL_OR_EMPTY);
+        }
+        this.name = name;
+    }
+
+    private void setSpecies(String species) {
         if (species == null || species.trim().isEmpty()) {
             throw new NullPointerException(SPECIES_NAME_NULL_OR_EMPTY);
         }
         this.species = species;
     }
 
-    public void setPrice(double price) {
+    private void setPrice(double price) {
         if (price <= 0) {
             throw new IllegalArgumentException(FISH_PRICE_BELOW_OR_EQUAL_ZERO);
         }
         this.price = price;
     }
 
-    @Override
-    public void setName(String name) {
-        if (name == null || name.trim().isEmpty()) {
-            throw new IllegalArgumentException(FISH_NAME_NULL_OR_EMPTY);
-        }
-        this.name = name;
+    protected void setSize(int size) {
+        this.size = size;
     }
 
     @Override
     public void eat() {
-        this.size += 5;
+        this.setSize(this.size + 5);
     }
 
     @Override

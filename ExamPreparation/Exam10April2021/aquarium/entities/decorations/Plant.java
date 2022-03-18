0952afa21026e123ca53fc24f4b85f4aada37ecc
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-18 11:59:34 +0200

    Update Plant.java

--- ExamPreparation/Exam10April2021/aquarium/entities/decorations/Plant.java
+++ ExamPreparation/Exam10April2021/aquarium/entities/decorations/Plant.java
@@ -1,10 +1,11 @@
-package M04_JavaOOP.ExamPreparation.Exam10April2021.aquarium.entities.decorations;
+package aquarium.entities.decorations;
 
 public class Plant extends BaseDecoration {
-    private final static int COMFORT = 5;
-    private final static double DECORATION = 10;
+
+    private static final int COMFORT = 5;
+    private static final double PRICE = 10;
 
     public Plant() {
-        super(COMFORT, DECORATION);
+        super(COMFORT, PRICE);
     }
 }

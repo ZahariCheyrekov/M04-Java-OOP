4cad3720b273a37ab76683c1607e5a70045b85eb
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-18 11:58:02 +0200

    Update SaltwaterAquarium.java

--- ExamPreparation/Exam10April2021/aquarium/entities/aquariums/SaltwaterAquarium.java
+++ ExamPreparation/Exam10April2021/aquarium/entities/aquariums/SaltwaterAquarium.java
@@ -1,7 +1,8 @@
-package M04_JavaOOP.ExamPreparation.Exam10April2021.aquarium.entities.aquariums;
+package aquarium.entities.aquariums;
 
 public class SaltwaterAquarium extends BaseAquarium {
-    private final static int CAPACITY = 25;
+
+    private static final int CAPACITY = 25;
 
     public SaltwaterAquarium(String name) {
         super(name, CAPACITY);

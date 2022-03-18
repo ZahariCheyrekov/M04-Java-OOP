707d243be05b8561480ff81e2360bb9bb256f12f
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-18 11:59:13 +0200

    Update Ornament.java

--- ExamPreparation/Exam10April2021/aquarium/entities/decorations/Ornament.java
+++ ExamPreparation/Exam10April2021/aquarium/entities/decorations/Ornament.java
@@ -1,10 +1,11 @@
-package M04_JavaOOP.ExamPreparation.Exam10April2021.aquarium.entities.decorations;
+package aquarium.entities.decorations;
 
 public class Ornament extends BaseDecoration {
-    private final static int COMFORT = 1;
-    private final static double DECORATION = 5;
+
+    private static final int COMFORT = 1;
+    private static final double PRICE = 5;
 
     public Ornament() {
-        super(COMFORT, DECORATION);
+        super(COMFORT, PRICE);
     }
 }

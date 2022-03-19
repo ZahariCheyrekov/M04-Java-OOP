5895609e62c75fc63e5232f39d8e4af9a028f621
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-19 17:01:36 +0200

    Update ComputerManagerTests.java

--- ExamPreparation/Exam16August2020/onlineShop/ComputerManagerTests.java
+++ ExamPreparation/Exam16August2020/onlineShop/ComputerManagerTests.java
@@ -8,10 +8,6 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 public class ComputerManagerTests {
-
-    private ComputerManager computerManager;
-    private Computer computer;
-
    
 
     @Test(expected = UnsupportedOperationException.class)

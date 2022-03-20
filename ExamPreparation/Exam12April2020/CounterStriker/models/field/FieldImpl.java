bfd83df9e51a8037de5a06df042fc639271c479f
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-20 11:19:39 +0200

    Update FieldImpl.java

--- ExamPreparation/Exam12April2020/CounterStriker/models/field/FieldImpl.java
+++ ExamPreparation/Exam12April2020/CounterStriker/models/field/FieldImpl.java
@@ -1,37 +1,32 @@
-package M04_JavaOOP.ExamPreparation.Exam12April2020.CounterStriker.models.field;
+package CounterStriker.models.field;
 
-import M04_JavaOOP.ExamPreparation.Exam12April2020.CounterStriker.models.players.CounterTerrorist;
-import M04_JavaOOP.ExamPreparation.Exam12April2020.CounterStriker.models.players.Player;
-import M04_JavaOOP.ExamPreparation.Exam12April2020.CounterStriker.models.players.Terrorist;
+import CounterStriker.models.players.Player;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.List;
 import java.util.stream.Collectors;
 
-import static M04_JavaOOP.ExamPreparation.Exam12April2020.CounterStriker.common.OutputMessages.*;
+import static CounterStriker.common.OutputMessages.*;
 
 public class FieldImpl implements Field {
+
     @Override
     public String start(Collection<Player> players) {
-        List<Player> contraTerrorists = players
-                .stream()
-                .filter(p -> p instanceof CounterTerrorist)
-                .collect(Collectors.toList());
 
-        List<Player> terrorists = players
-                .stream()
-                .filter(p -> p instanceof Terrorist)
-                .collect(Collectors.toList());
+        List<Player> terrorists = getPlayersByType(players, "Terrorist");
+        List<Player> counterTerrorists = getPlayersByType(players, "CounterTerrorist");
 
-        while (contraTerrorists.stream().anyMatch(Player::isAlive) && terrorists.stream().anyMatch(Player::isAlive)) {
+
+        while (counterTerrorists.stream().anyMatch(Player::isAlive) && terrorists.stream().anyMatch(Player::isAlive)) {
             for (Player terrorist : terrorists) {
-                for (Player contraTerrorist : contraTerrorists) {
+                for (Player contraTerrorist : counterTerrorists) {
                     contraTerrorist.takeDamage(terrorist.getGun().fire());
                 }
             }
 
-            contraTerrorists = contraTerrorists.stream().filter(Player::isAlive).collect(Collectors.toList());
+            counterTerrorists = counterTerrorists.stream().filter(Player::isAlive).collect(Collectors.toList());
 
-            for (Player contraTerrorist : contraTerrorists) {
+            for (Player contraTerrorist : counterTerrorists) {
                 for (Player terrorist : terrorists) {
                     terrorist.takeDamage(contraTerrorist.getGun().fire());
                 }
@@ -39,6 +34,17 @@ public class FieldImpl implements Field {
 
             terrorists = terrorists.stream().filter(Player::isAlive).collect(Collectors.toList());
         }
+
         return terrorists.stream().anyMatch(Player::isAlive) ? TERRORIST_WINS : COUNTER_TERRORIST_WINS;
+    }
+
+    private List<Player> getPlayersByType(Collection<Player> players, String type) {
+        return players
+                .stream()
+                .filter(player -> player
+                        .getClass()
+                        .getSimpleName()
+                        .equals(type))
+                .collect(Collectors.toList());
     }
 }

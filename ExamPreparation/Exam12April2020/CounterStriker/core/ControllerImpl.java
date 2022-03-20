3a64e76c15deab902a3906a6ff4ee043002095d7
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-20 11:19:08 +0200

    Update ControllerImpl.java

--- ExamPreparation/Exam12April2020/CounterStriker/core/ControllerImpl.java
+++ ExamPreparation/Exam12April2020/CounterStriker/core/ControllerImpl.java
@@ -1,97 +1,115 @@
-package M04_JavaOOP.ExamPreparation.Exam12April2020.CounterStriker.core;
+package CounterStriker.core;
 
-import M04_JavaOOP.ExamPreparation.Exam12April2020.CounterStriker.models.field.Field;
-import M04_JavaOOP.ExamPreparation.Exam12April2020.CounterStriker.models.field.FieldImpl;
-import M04_JavaOOP.ExamPreparation.Exam12April2020.CounterStriker.models.guns.Gun;
-import M04_JavaOOP.ExamPreparation.Exam12April2020.CounterStriker.models.guns.Pistol;
-import M04_JavaOOP.ExamPreparation.Exam12April2020.CounterStriker.models.guns.Rifle;
-import M04_JavaOOP.ExamPreparation.Exam12April2020.CounterStriker.models.players.CounterTerrorist;
-import M04_JavaOOP.ExamPreparation.Exam12April2020.CounterStriker.models.players.Player;
-import M04_JavaOOP.ExamPreparation.Exam12April2020.CounterStriker.models.players.Terrorist;
-import M04_JavaOOP.ExamPreparation.Exam12April2020.CounterStriker.repositories.GunRepository;
-import M04_JavaOOP.ExamPreparation.Exam12April2020.CounterStriker.repositories.PlayerRepository;
-import M04_JavaOOP.ExamPreparation.Exam12April2020.CounterStriker.repositories.Repository;
+import CounterStriker.models.field.Field;
+import CounterStriker.models.field.FieldImpl;
+import CounterStriker.models.guns.Gun;
+import CounterStriker.models.guns.Pistol;
+import CounterStriker.models.guns.Rifle;
+import CounterStriker.models.players.CounterTerrorist;
+import CounterStriker.models.players.Player;
+import CounterStriker.models.players.Terrorist;
+import CounterStriker.repositories.GunRepository;
+import CounterStriker.repositories.PlayerRepository;
 
-import java.util.Collection;
+import java.util.List;
 import java.util.stream.Collectors;
 
-import static M04_JavaOOP.ExamPreparation.Exam12April2020.CounterStriker.common.ExceptionMessages.*;
-import static M04_JavaOOP.ExamPreparation.Exam12April2020.CounterStriker.common.OutputMessages.*;
+import static CounterStriker.common.ExceptionMessages.*;
+import static CounterStriker.common.OutputMessages.*;
 
 public class ControllerImpl implements Controller {
-    private Repository<Gun> guns;
-    private Repository<Player> players;
+
+    private GunRepository guns;
+    private PlayerRepository players;
     private Field field;
 
     public ControllerImpl() {
-        this.guns = new GunRepository<>();
-        this.players = new PlayerRepository<>();
+        this.guns = new GunRepository();
+        this.players = new PlayerRepository();
         this.field = new FieldImpl();
     }
 
     @Override
     public String addGun(String type, String name, int bulletsCount) {
         Gun gun;
-        if ("Pistol".equals(type)) {
-            gun = new Pistol(name, bulletsCount);
-        } else if ("Rifle".equals(type)) {
-            gun = new Rifle(name, bulletsCount);
-        } else {
-            throw new IllegalArgumentException(INVALID_GUN_TYPE);
+
+        switch (type) {
+            case "Pistol":
+                gun = new Pistol(name, bulletsCount);
+                break;
+            case "Rifle":
+                gun = new Rifle(name, bulletsCount);
+                break;
+            default:
+                throw new IllegalArgumentException(INVALID_GUN_TYPE);
         }
+
         this.guns.add(gun);
+
         return String.format(SUCCESSFULLY_ADDED_GUN, name);
     }
 
     @Override
     public String addPlayer(String type, String username, int health, int armor, String gunName) {
         Gun gun = this.guns.findByName(gunName);
+
         if (gun == null) {
             throw new NullPointerException(GUN_CANNOT_BE_FOUND);
         }
+
         Player player;
-        if ("CounterTerrorist".equals(type)) {
-            player = new CounterTerrorist(username, health, armor, gun);
-        } else if ("Terrorist".equals(type)) {
-            player = new Terrorist(username, health, armor, gun);
-        } else {
-            throw new IllegalArgumentException(INVALID_PLAYER_TYPE);
+
+        switch (type) {
+            case "CounterTerrorist":
+                player = new CounterTerrorist(username, health, armor, gun);
+                break;
+            case "Terrorist":
+                player = new Terrorist(username, health, armor, gun);
+                break;
+            default:
+                throw new IllegalArgumentException(INVALID_PLAYER_TYPE);
         }
+
         this.players.add(player);
+
         return String.format(SUCCESSFULLY_ADDED_PLAYER, username);
     }
 
     @Override
     public String startGame() {
-        Collection<Player> players = this
-                .players
+        List<Player> alivePlayers = this.players
                 .getModels()
                 .stream()
                 .filter(Player::isAlive)
                 .collect(Collectors.toList());
-        return this.field.start(players);
+
+        return field.start(alivePlayers);
     }
 
     @Override
     public String report() {
-        StringBuilder sb = new StringBuilder();
-        this
-                .players
+        StringBuilder info = new StringBuilder();
+
+        this.players
                 .getModels()
                 .stream()
-                .sorted((p1, p2) -> {
-                    int result = p1.getClass().getSimpleName().compareTo(p2.getClass().getSimpleName());
+                .sorted((first, second) -> {
+                    int result = first.getClass().getSimpleName().compareTo(second.getClass().getSimpleName());
+
                     if (result == 0) {
-                        result = Integer.compare(p2.getHealth(), p1.getHealth());
+                        result = Integer.compare(second.getHealth(), first.getHealth());
                     }
+
                     if (result == 0) {
-                        result = p1.getUsername().compareTo(p2.getUsername());
+                        result = first.getUsername().compareTo(second.getUsername());
                     }
+
                     return result;
                 })
-                .forEach(p -> sb
+                .forEach(p -> info
                         .append(p)
                         .append(System.lineSeparator()));
-        return sb.toString().trim();
+
+        return info.toString().trim();
     }
 }

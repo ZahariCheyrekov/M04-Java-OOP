92809d41c5fad448e142d87c25195771583496f6
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-20 11:20:52 +0200

    Update Rifle.java

--- ExamPreparation/Exam12April2020/CounterStriker/models/guns/Rifle.java
+++ ExamPreparation/Exam12April2020/CounterStriker/models/guns/Rifle.java
@@ -1,7 +1,8 @@
-package M04_JavaOOP.ExamPreparation.Exam12April2020.CounterStriker.models.guns;
+package CounterStriker.models.guns;
 
 public class Rifle extends GunImpl {
-    private static final int BULLETS_TO_SHOOT = 10;
+
+    private static final int BULLETS_TO_FIRE = 10;
 
     public Rifle(String name, int bulletsCount) {
         super(name, bulletsCount);
@@ -9,10 +10,10 @@ public class Rifle extends GunImpl {
 
     @Override
     public int fire() {
-        if (super.getBulletsCount() < BULLETS_TO_SHOOT) {
+        if (super.getBulletsCount() < BULLETS_TO_FIRE) {
             return 0;
         }
-        super.decreaseBullets(BULLETS_TO_SHOOT);
-        return BULLETS_TO_SHOOT;
+        super.decreaseBullets(BULLETS_TO_FIRE);
+        return BULLETS_TO_FIRE;
     }
 }

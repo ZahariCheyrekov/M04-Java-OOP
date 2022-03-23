f9a460d78d068bc20403b4b5854b6a7c4d5df4ef
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-23 11:13:14 +0200

    Update BaseCar.java

--- ExamPreparation/Exam20December2021/christmasRaces/entities/cars/BaseCar.java
+++ ExamPreparation/Exam20December2021/christmasRaces/entities/cars/BaseCar.java
@@ -1,35 +1,23 @@
-package M04_JavaOOP.ExamPreparation.Exam20December2021.christmasRaces.entities.cars;
+package christmasRaces.entities.cars;
+
+import static christmasRaces.common.ExceptionMessages.*;
 
 public abstract class BaseCar implements Car {
+
     private String model;
     private int horsePower;
     private double cubicCentimeters;
 
-    public BaseCar(String model, int horsePower, double cubicCentimeters) {
-        setModel(model);
-        setHorsePower(horsePower);
-        setCubicCentimeters(cubicCentimeters);
+    protected BaseCar(String model, int horsePower, double cubicCentimeters) {
+        this.setModel(model);
+        this.setHorsePower(horsePower);
+        this.cubicCentimeters = cubicCentimeters;
     }
 
-    @Override
-    public String getModel() {
-        return model;
-    }
-
-    @Override
-    public int getHorsePower() {
-        return horsePower;
-    }
-
-    @Override
-    public double getCubicCentimeters() {
-        return cubicCentimeters;
-    }
-
-    protected void setModel(String model) {
-        if (model.trim().length() < 4)
-            throw new IllegalArgumentException(String.format(
-                    "Model %s cannot be less than 4 symbols.", model));
+    private void setModel(String model) {
+        if (model == null || model.trim().isEmpty()) {
+            throw new IllegalArgumentException(String.format(INVALID_MODEL, model, 4));
+        }
         this.model = model;
     }
 
@@ -37,11 +25,23 @@ public abstract class BaseCar implements Car {
         this.horsePower = horsePower;
     }
 
-    protected void setCubicCentimeters(double cubicCentimeters) {
-        this.cubicCentimeters = cubicCentimeters;
+    @Override
+    public String getModel() {
+        return this.model;
     }
 
+    @Override
+    public int getHorsePower() {
+        return this.horsePower;
+    }
+
+    @Override
+    public double getCubicCentimeters() {
+        return this.cubicCentimeters;
+    }
+
+    @Override
     public double calculateRacePoints(int laps) {
-        return cubicCentimeters / horsePower * laps;
+        return this.cubicCentimeters / this.horsePower * laps;
     }
 }

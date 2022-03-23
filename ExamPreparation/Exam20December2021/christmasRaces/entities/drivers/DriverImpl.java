dee09bd809da251ee3e3afb9509de1ad6bf4ac88
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-23 11:14:20 +0200

    Update DriverImpl.java

--- ExamPreparation/Exam20December2021/christmasRaces/entities/drivers/DriverImpl.java
+++ ExamPreparation/Exam20December2021/christmasRaces/entities/drivers/DriverImpl.java
@@ -1,53 +1,60 @@
-package M04_JavaOOP.ExamPreparation.Exam20December2021.christmasRaces.entities.drivers;
+package christmasRaces.entities.drivers;
 
-import M04_JavaOOP.ExamPreparation.Exam20December2021.christmasRaces.entities.cars.Car;
+import christmasRaces.entities.cars.Car;
+
+import static christmasRaces.common.ExceptionMessages.CAR_INVALID;
+import static christmasRaces.common.ExceptionMessages.INVALID_NAME;
 
 public class DriverImpl implements Driver {
+
     private String name;
     private Car car;
     private int numberOfWins;
     private boolean canParticipate;
 
     public DriverImpl(String name) {
-        setName(name);
-        car = null;
-        numberOfWins = 0;
-        canParticipate = false;
+        this.setName(name);
+    }
+
+    private void setName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException(String.format(INVALID_NAME, name, 5));
+        }
+        this.name = name;
     }
 
     @Override
     public String getName() {
-        return name;
+        return this.name;
     }
 
     @Override
     public Car getCar() {
-        return car;
+        return this.car;
     }
 
     @Override
     public int getNumberOfWins() {
-        return numberOfWins;
+        return this.numberOfWins;
     }
 
-    public boolean getCanParticipate() {
-        return canParticipate;
-    }
-
-    private void setName(String name) {
-        if (name.trim().length() < 5)
-            throw new IllegalArgumentException(String.format("Name %s cannot be less than 5 symbols.", name));
-        this.name = name;
-    }
-
+    @Override
     public void addCar(Car car) {
-        if(car == null)
-            throw new IllegalArgumentException("Car cannot be null.");
+        if (car == null) {
+            throw new IllegalArgumentException(CAR_INVALID);
+        }
+
         this.car = car;
         this.canParticipate = true;
     }
 
+    @Override
     public void winRace() {
         this.numberOfWins++;
+    }
+
+    @Override
+    public boolean getCanParticipate() {
+        return this.canParticipate;
     }
 }

b41d6d406d37baba1701d10840f0560b1ee84be1
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-23 11:12:38 +0200

    Update ControllerImpl.java

--- ExamPreparation/Exam20December2021/christmasRaces/core/ControllerImpl.java
+++ ExamPreparation/Exam20December2021/christmasRaces/core/ControllerImpl.java
@@ -1,23 +1,26 @@
-package M04_JavaOOP.ExamPreparation.Exam20December2021.christmasRaces.core;
+package christmasRaces.core;
 
-import M04_JavaOOP.ExamPreparation.Exam20December2021.christmasRaces.core.interfaces.Controller;
-import M04_JavaOOP.ExamPreparation.Exam20December2021.christmasRaces.entities.cars.Car;
-import M04_JavaOOP.ExamPreparation.Exam20December2021.christmasRaces.entities.cars.MuscleCar;
-import M04_JavaOOP.ExamPreparation.Exam20December2021.christmasRaces.entities.cars.SportsCar;
-import M04_JavaOOP.ExamPreparation.Exam20December2021.christmasRaces.entities.drivers.Driver;
-import M04_JavaOOP.ExamPreparation.Exam20December2021.christmasRaces.entities.drivers.DriverImpl;
-import M04_JavaOOP.ExamPreparation.Exam20December2021.christmasRaces.entities.races.Race;
-import M04_JavaOOP.ExamPreparation.Exam20December2021.christmasRaces.entities.races.RaceImpl;
-import M04_JavaOOP.ExamPreparation.Exam20December2021.christmasRaces.repositories.interfaces.Repository;
+import christmasRaces.core.interfaces.Controller;
+import christmasRaces.entities.cars.Car;
+import christmasRaces.entities.cars.MuscleCar;
+import christmasRaces.entities.cars.SportsCar;
+import christmasRaces.entities.drivers.Driver;
+import christmasRaces.entities.drivers.DriverImpl;
+import christmasRaces.entities.races.Race;
+import christmasRaces.entities.races.RaceImpl;
+import christmasRaces.repositories.interfaces.Repository;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static M04_JavaOOP.ExamPreparation.Exam20December2021.christmasRaces.common.ExceptionMessages.RACE_INVALID;
-import static M04_JavaOOP.ExamPreparation.Exam20December2021.christmasRaces.common.ExceptionMessages.RACE_NOT_FOUND;
+import static christmasRaces.common.ExceptionMessages.*;
+import static christmasRaces.common.OutputMessages.*;
 
 public class ControllerImpl implements Controller {
+
     private Repository<Driver> driverRepository;
     private Repository<Car> carRepository;
     private Repository<Race> raceRepository;
@@ -29,89 +32,128 @@ public class ControllerImpl implements Controller {
     }
 
     @Override
-    public String createDriver(String driverName) {
-        if(driverRepository.getByName(driverName) != null)
-            throw new IllegalArgumentException(String.format("Driver %s is already created.", driverName));
+    public String createDriver(String driver) {
 
-        driverRepository.add(new DriverImpl(driverName));
-        return String.format("Driver %s is created.", driverName);
+        Driver existingDriver = this.driverRepository.getByName(driver);
+
+        if (existingDriver != null) {
+            throw new IllegalArgumentException(String.format(DRIVER_EXISTS, driver));
+        }
+
+        Driver newDriver = new DriverImpl(driver);
+        this.driverRepository.add(newDriver);
+
+        return String.format(DRIVER_CREATED, driver);
     }
 
     @Override
     public String createCar(String type, String model, int horsePower) {
-        if (carRepository.getByName(model) != null)
-            throw new IllegalArgumentException(String.format("Car %s is already created.", model));
+
+        Car existingCar = this.carRepository.getByName(model);
+
+        if (existingCar != null) {
+            throw new IllegalArgumentException(String.format(CAR_EXISTS, model));
+        }
+
+        Car newCar = null;
+        String carType = "";
+
         switch (type) {
             case "Muscle":
-                carRepository.add(new MuscleCar(model, horsePower));
-                return String.format("MuscleCar %s is created.", model);
+                newCar = new MuscleCar(model, horsePower);
+                carType = "MuscleCar";
+                break;
             case "Sports":
-                carRepository.add(new SportsCar(model, horsePower));
-                return String.format("SportsCar %s is created.", model);
+                newCar = new SportsCar(model, horsePower);
+                carType = "SportsCar";
+                break;
         }
-        return null;
+
+        this.carRepository.add(newCar);
+
+        return String.format(CAR_CREATED, carType, model);
     }
 
     @Override
     public String addCarToDriver(String driverName, String carModel) {
-        Driver driver = driverRepository.getByName(driverName);
-        if (driver == null)
-            throw new IllegalArgumentException(String.format("Driver %s could not be found.", driverName));
-        Car car = carRepository.getByName(carModel);
-        if(car == null)
-            throw new IllegalArgumentException(String.format("Car %s could not be found.", carModel));
+
+        Driver driver = this.driverRepository.getByName(driverName);
+
+        if (driver == null) {
+            throw new IllegalArgumentException(String.format(DRIVER_NOT_FOUND, driverName));
+        }
+
+        Car car = this.carRepository.getByName(carModel);
+
+        if (car == null) {
+            throw new IllegalArgumentException(String.format(CAR_NOT_FOUND, carModel));
+        }
+
         driver.addCar(car);
-        return String.format("Driver %s received car %s.", driverName, carModel);
+
+        return String.format(CAR_ADDED, driverName, carModel);
     }
 
     @Override
     public String addDriverToRace(String raceName, String driverName) {
-        Race race = raceRepository.getByName(raceName);
-        if (race == null)
-            throw new IllegalArgumentException(String.format("Race %s could not be found.", raceName));
-        Driver driver = driverRepository.getByName(driverName);
-        if (driver == null)
-            throw new IllegalArgumentException(String.format("Driver %s could not be found.", driverName));
-        race.addDriver(driver);
-        return String.format("Driver %s added in %s race.", driverName, raceName);
-    }
 
-    @Override
-    public String startRace(String raceName) {
         Race race = this.raceRepository.getByName(raceName);
 
         if (race == null) {
             throw new IllegalArgumentException(String.format(RACE_NOT_FOUND, raceName));
         }
 
-        Collection<Driver> participants = race.getDrivers();
+        Driver driver = this.driverRepository.getByName(driverName);
 
-        if (participants.size() < 3) {
+        if (driver == null) {
+            throw new IllegalArgumentException(String.format(DRIVER_NOT_FOUND, driverName));
+        }
+
+        race.addDriver(driver);
+
+        return String.format(DRIVER_ADDED, driverName, raceName);
+    }
+
+    @Override
+    public String startRace(String raceName) {
+
+        Race race = this.raceRepository.getByName(raceName);
+
+        if (race == null) {
+            throw new IllegalArgumentException(String.format(RACE_NOT_FOUND, raceName));
+        }
+
+        int driversInTheRace = race.getDrivers().size();
+
+        if (driversInTheRace < 3) {
             throw new IllegalArgumentException(String.format(RACE_INVALID, raceName, 3));
         }
 
         int laps = race.getLaps();
+        Collection<Driver> drivers = race.getDrivers();
 
-        List<Driver> fastestThree = participants
+        List<Driver> fastestThree = drivers
                 .stream()
-                .sorted((p2, p1) -> Double.compare(p1.getCar().calculateRacePoints(laps),
-                        p2.getCar().calculateRacePoints(laps)))
+                .sorted((d2, d1) -> Double.compare(d1.getCar().calculateRacePoints(laps),
+                        d2.getCar().calculateRacePoints(laps)))
                 .limit(3)
                 .collect(Collectors.toList());
 
         this.raceRepository.remove(race);
 
-        StringBuilder message = new StringBuilder();
-
         Driver first = fastestThree.get(0);
         Driver second = fastestThree.get(1);
         Driver third = fastestThree.get(2);
 
-        message.append(String.format("Driver %s wins %s race.", first.getName(), raceName))
+        StringBuilder message = new StringBuilder();
+
+        String nameRace = race.getName();
+
+        message.append(String.format(DRIVER_FIRST_POSITION, first.getName(), nameRace))
                 .append(System.lineSeparator());
-        message.append(String.format("Driver %s is second in %s race.", second.getName(), raceName))
+        message.append(String.format(DRIVER_SECOND_POSITION, second.getName(), nameRace))
                 .append(System.lineSeparator());
-        message.append(String.format("Driver %s is third in %s race.", third.getName(), raceName))
+        message.append(String.format(DRIVER_THIRD_POSITION, third.getName(), nameRace))
                 .append(System.lineSeparator());
 
         return message.toString().trim();
@@ -119,9 +161,16 @@ public class ControllerImpl implements Controller {
 
     @Override
     public String createRace(String name, int laps) {
-        if(raceRepository.getByName(name) != null)
-            throw new IllegalArgumentException(String.format("Race %s is already created.", name));
-        raceRepository.add(new RaceImpl(name, laps));
-        return String.format("Race %s is created.", name);
+
+        Race race = this.raceRepository.getByName(name);
+
+        if (race != null) {
+            throw new IllegalArgumentException(String.format(RACE_EXISTS, name));
+        }
+
+        Race newRace = new RaceImpl(name, laps);
+        this.raceRepository.add(newRace);
+
+        return String.format(RACE_CREATED, name);
     }
 }

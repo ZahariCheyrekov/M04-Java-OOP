e0e1642c92e9791a5335ff36c85aa4c387a7bff9
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-23 11:16:44 +0200

    Update RaceRepository.java

--- ExamPreparation/Exam20December2021/christmasRaces/repositories/RaceRepository.java
+++ ExamPreparation/Exam20December2021/christmasRaces/repositories/RaceRepository.java
@@ -1,36 +1,35 @@
-package M04_JavaOOP.ExamPreparation.Exam20December2021.christmasRaces.repositories;
+package christmasRaces.repositories;
 
-import M04_JavaOOP.ExamPreparation.Exam20December2021.christmasRaces.entities.races.Race;
-import M04_JavaOOP.ExamPreparation.Exam20December2021.christmasRaces.repositories.interfaces.Repository;
+import christmasRaces.entities.races.Race;
+import christmasRaces.repositories.interfaces.Repository;
 
 import java.util.*;
 
 public class RaceRepository implements Repository<Race> {
-    private final Map<String, Race> races;
+
+    private Map<String, Race> races;
 
     public RaceRepository() {
-        races = new LinkedHashMap<>();
+        this.races = new LinkedHashMap<>();
     }
 
     @Override
     public Race getByName(String name) {
-        return races.get(name);
+        return this.races.get(name);
     }
 
     @Override
     public Collection<Race> getAll() {
-        return Collections.unmodifiableCollection(races.values());
+        return Collections.unmodifiableCollection(this.races.values());
     }
 
     @Override
-    public void add(Race race) {
-        races.put(race.getName(), race);
+    public void add(Race model) {
+        this.races.put(model.getName(), model);
     }
 
     @Override
     public boolean remove(Race model) {
-        if(null == races.remove(model.getName()))
-            return false;
-        return true;
+        return this.races.remove(model.getName()) != null;
     }
 }

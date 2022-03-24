9c522662afa1113089f38197ea52f127ea376556
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-24 18:16:43 +0200

    Update ShortHouse.java

--- ExamPreparation/Exam11December2021/catHouse/entities/houses/ShortHouse.java
+++ ExamPreparation/Exam11December2021/catHouse/entities/houses/ShortHouse.java
@@ -1,6 +1,7 @@
-package M04_JavaOOP.ExamPreparation.Exam11December2021.catHouse.entities.houses;
+package catHouse.entities.houses;
 
 public class ShortHouse extends BaseHouse {
+
     private static final int CAPACITY = 15;
 
     public ShortHouse(String name) {

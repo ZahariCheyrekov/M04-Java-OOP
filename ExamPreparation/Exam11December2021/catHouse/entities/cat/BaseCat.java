82a672c42c3e25cc34899fdc389abd53341812cc
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-24 18:14:54 +0200

    Update BaseCat.java

--- ExamPreparation/Exam11December2021/catHouse/entities/cat/BaseCat.java
+++ ExamPreparation/Exam11December2021/catHouse/entities/cat/BaseCat.java
@@ -1,8 +1,11 @@
-package M04_JavaOOP.ExamPreparation.Exam11December2021.catHouse.entities.cat;
+package catHouse.entities.cat;
 
-import M04_JavaOOP.ExamPreparation.Exam11December2021.catHouse.common.ExceptionMessages;
+import catHouse.common.DataValidator;
+
+import static catHouse.common.ExceptionMessages.*;
 
 public abstract class BaseCat implements Cat {
+
     private String name;
     private String breed;
     private int kilograms;
@@ -14,24 +17,6 @@ public abstract class BaseCat implements Cat {
         this.setPrice(price);
     }
 
-    protected void setKilograms(int kilograms) {
-        this.kilograms = kilograms;
-    }
-
-    private void setBreed(String breed) {
-        if (breed == null || breed.trim().isEmpty()) {
-            throw new NullPointerException(ExceptionMessages.CAT_BREED_CANNOT_BE_NULL_OR_EMPTY);
-        }
-        this.breed = breed;
-    }
-
-    private void setPrice(double price) {
-        if (price <= 0) {
-            throw new IllegalArgumentException(ExceptionMessages.CAT_PRICE_CANNOT_BE_BELOW_OR_EQUAL_TO_ZERO);
-        }
-        this.price = price;
-    }
-
     @Override
     public String getName() {
         return this.name;
@@ -39,10 +24,18 @@ public abstract class BaseCat implements Cat {
 
     @Override
     public void setName(String name) {
-        if (name == null || name.trim().isEmpty()) {
-            throw new NullPointerException(ExceptionMessages.CAT_NAME_NULL_OR_EMPTY);
-        }
+        DataValidator.validateStringData(name, CAT_NAME_NULL_OR_EMPTY);
         this.name = name;
+    }
+
+    private void setBreed(String breed) {
+        DataValidator.validateStringData(breed, CAT_BREED_CANNOT_BE_NULL_OR_EMPTY);
+        this.breed = breed;
+    }
+
+    private void setPrice(double price) {
+        DataValidator.validateCatPrice(price, CAT_PRICE_CANNOT_BE_BELOW_OR_EQUAL_TO_ZERO);
+        this.price = price;
     }
 
     @Override
@@ -55,8 +48,7 @@ public abstract class BaseCat implements Cat {
         return this.price;
     }
 
-    @Override
-    public void eating() {
-        this.kilograms += 1;
+    protected void setKilograms(int kilograms) {
+        this.kilograms = kilograms;
     }
 }

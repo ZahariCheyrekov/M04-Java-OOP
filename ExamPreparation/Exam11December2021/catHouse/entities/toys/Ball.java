a5fc5fc10c82ebfd6b9e7dd695cfbfb4b2a58f46
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-24 18:16:57 +0200

    Update Ball.java

--- ExamPreparation/Exam11December2021/catHouse/entities/toys/Ball.java
+++ ExamPreparation/Exam11December2021/catHouse/entities/toys/Ball.java
@@ -1,6 +1,7 @@
-package M04_JavaOOP.ExamPreparation.Exam11December2021.catHouse.entities.toys;
+package catHouse.entities.toys;
 
 public class Ball extends BaseToy {
+
     private static final int SOFTNESS = 1;
     private static final double PRICE = 10;
 

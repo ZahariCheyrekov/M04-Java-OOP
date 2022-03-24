c1a78f5b193d60e23d69b2c8d4942bb44e67af97
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-24 18:16:31 +0200

    Update LongHouse.java

--- ExamPreparation/Exam11December2021/catHouse/entities/houses/LongHouse.java
+++ ExamPreparation/Exam11December2021/catHouse/entities/houses/LongHouse.java
@@ -1,6 +1,7 @@
-package M04_JavaOOP.ExamPreparation.Exam11December2021.catHouse.entities.houses;
+package catHouse.entities.houses;
 
 public class LongHouse extends BaseHouse {
+
     private static final int CAPACITY = 30;
 
     public LongHouse(String name) {

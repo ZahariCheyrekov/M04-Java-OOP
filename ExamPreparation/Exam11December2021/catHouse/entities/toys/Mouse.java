ad93c8a58b82fc666edcf05ea8d9e8d9b61fa032
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-24 18:17:20 +0200

    Update Mouse.java

--- ExamPreparation/Exam11December2021/catHouse/entities/toys/Mouse.java
+++ ExamPreparation/Exam11December2021/catHouse/entities/toys/Mouse.java
@@ -1,6 +1,7 @@
-package M04_JavaOOP.ExamPreparation.Exam11December2021.catHouse.entities.toys;
+package catHouse.entities.toys;
 
 public class Mouse extends BaseToy {
+
     private static final int SOFTNESS = 5;
     private static final double PRICE = 15;
 

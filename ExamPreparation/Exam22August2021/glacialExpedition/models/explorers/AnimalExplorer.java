75ba07b6bfcb657ac3c00fe0d34d06d637661d36
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-25 09:54:57 +0200

    Update AnimalExplorer.java

--- ExamPreparation/Exam22August2021/glacialExpedition/models/explorers/AnimalExplorer.java
+++ ExamPreparation/Exam22August2021/glacialExpedition/models/explorers/AnimalExplorer.java
@@ -1,9 +1,10 @@
-package M04_JavaOOP.ExamPreparation.Exam22August2021.glacialExpedition.models.explorers;
+package glacialExpedition.models.explorers;
 
-public class AnimalExplorer extends BaseExplorer{
-    private static final double ANIMAL_EXPLORER_ENERGY = 100;
+public class AnimalExplorer extends BaseExplorer {
+
+    private static final double ENERGY = 100;
 
     public AnimalExplorer(String name) {
-        super(name, ANIMAL_EXPLORER_ENERGY);
+        super(name, ENERGY);
     }
 }

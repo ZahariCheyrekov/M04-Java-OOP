61d6757e310fc5fb4a1b89de1bfcb27d3d59c89f
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-25 09:55:45 +0200

    Update BaseExplorer.java

--- ExamPreparation/Exam22August2021/glacialExpedition/models/explorers/BaseExplorer.java
+++ ExamPreparation/Exam22August2021/glacialExpedition/models/explorers/BaseExplorer.java
@@ -1,60 +1,80 @@
-package M04_JavaOOP.ExamPreparation.Exam22August2021.glacialExpedition.models.explorers;
+package glacialExpedition.models.explorers;
 
-import M04_JavaOOP.ExamPreparation.Exam22August2021.glacialExpedition.models.suitcases.Carton;
-import M04_JavaOOP.ExamPreparation.Exam22August2021.glacialExpedition.models.suitcases.Suitcase;
-import M04_JavaOOP.ExamPreparation.Exam22August2021.glacialExpedition.common.ExceptionMessages;
+import glacialExpedition.models.suitcases.Carton;
+import glacialExpedition.models.suitcases.Suitcase;
+
+import static glacialExpedition.common.ConstantMessages.*;
+import static glacialExpedition.common.ExceptionMessages.*;
 
 public abstract class BaseExplorer implements Explorer {
+
     private String name;
     private double energy;
     private Suitcase suitcase;
+    private static final double ZERO_ENERGY = 0;
+    private static final double DECREASE_IN_ENERGY = 15;
 
     protected BaseExplorer(String name, double energy) {
-        setName(name);
-        setEnergy(energy);
+        this.setName(name);
+        this.setEnergy(energy);
         this.suitcase = new Carton();
     }
 
-    @Override
-    public void search() {
-        this.energy = Math.max(0, this.energy - 15);
-    }
-
-    @Override
-    public boolean canSearch(){
-        return energy > 0;
-    }
-
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
+    private void setName(String name) {
         if (name == null || name.trim().isEmpty()) {
-            throw new NullPointerException(ExceptionMessages.EXPLORER_NAME_NULL_OR_EMPTY);
+            throw new NullPointerException(EXPLORER_NAME_NULL_OR_EMPTY);
         }
         this.name = name;
     }
 
-    @Override
-    public double getEnergy() {
-        return energy;
-    }
-
-    public void setEnergy(double energy) {
-        if (energy < 0) {
-            throw new IllegalArgumentException(ExceptionMessages.EXPLORER_ENERGY_LESS_THAN_ZERO);
+    protected void setEnergy(double energy) {
+        if (energy < ZERO_ENERGY) {
+            throw new IllegalArgumentException(EXPLORER_ENERGY_LESS_THAN_ZERO);
         }
         this.energy = energy;
     }
 
     @Override
-    public Suitcase getSuitcase() {
-        return suitcase;
+    public String getName() {
+        return this.name;
     }
 
-    public void setSuitcase(Suitcase suitcase) {
-        this.suitcase = suitcase;
+    @Override
+    public double getEnergy() {
+        return this.energy;
+    }
+
+    @Override
+    public boolean canSearch() {
+        return this.energy > ZERO_ENERGY;
+    }
+
+    @Override
+    public Suitcase getSuitcase() {
+        return this.suitcase;
+    }
+
+    @Override
+    public void search() {
+        this.energy = Math.max(ZERO_ENERGY, this.energy - DECREASE_IN_ENERGY);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder info = new StringBuilder();
+
+        info.append(String.format(FINAL_EXPLORER_NAME, this.name)).append(System.lineSeparator());
+        info.append(String.format(FINAL_EXPLORER_ENERGY, this.energy)).append(System.lineSeparator());
+        info.append("Suitcase exhibits: ");
+
+        int exhibitsCount = this.suitcase.getExhibits().size();
+
+        String suitcaseExhibit = exhibitsCount == 0
+                ? "None"
+                : String.join(FINAL_EXPLORER_SUITCASE_EXHIBITS_DELIMITER, this.getSuitcase().getExhibits());
+
+        info.append(suitcaseExhibit);
+
+        return info.toString().trim();
     }
 }

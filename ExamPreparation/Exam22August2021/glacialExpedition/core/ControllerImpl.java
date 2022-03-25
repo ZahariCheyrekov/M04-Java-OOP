35c66188823ee328b379cf64aa633a47b89348a3
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-25 09:54:18 +0200

    Update ControllerImpl.java

--- ExamPreparation/Exam22August2021/glacialExpedition/core/ControllerImpl.java
+++ ExamPreparation/Exam22August2021/glacialExpedition/core/ControllerImpl.java
@@ -1,27 +1,29 @@
-package M04_JavaOOP.ExamPreparation.Exam22August2021.glacialExpedition.core;
+package glacialExpedition.core;
 
-import M04_JavaOOP.ExamPreparation.Exam22August2021.glacialExpedition.common.ConstantMessages;
-import M04_JavaOOP.ExamPreparation.Exam22August2021.glacialExpedition.common.ExceptionMessages;
-import M04_JavaOOP.ExamPreparation.Exam22August2021.glacialExpedition.models.explorers.AnimalExplorer;
-import M04_JavaOOP.ExamPreparation.Exam22August2021.glacialExpedition.models.explorers.Explorer;
-import M04_JavaOOP.ExamPreparation.Exam22August2021.glacialExpedition.models.explorers.GlacierExplorer;
-import M04_JavaOOP.ExamPreparation.Exam22August2021.glacialExpedition.models.explorers.NaturalExplorer;
-import M04_JavaOOP.ExamPreparation.Exam22August2021.glacialExpedition.models.mission.Mission;
-import M04_JavaOOP.ExamPreparation.Exam22August2021.glacialExpedition.models.mission.MissionImpl;
-import M04_JavaOOP.ExamPreparation.Exam22August2021.glacialExpedition.models.states.State;
-import M04_JavaOOP.ExamPreparation.Exam22August2021.glacialExpedition.models.states.StateImpl;
-import M04_JavaOOP.ExamPreparation.Exam22August2021.glacialExpedition.repositories.ExplorerRepository;
-import M04_JavaOOP.ExamPreparation.Exam22August2021.glacialExpedition.repositories.Repository;
-import M04_JavaOOP.ExamPreparation.Exam22August2021.glacialExpedition.repositories.StateRepository;
+import glacialExpedition.models.explorers.AnimalExplorer;
+import glacialExpedition.models.explorers.Explorer;
+import glacialExpedition.models.explorers.GlacierExplorer;
+import glacialExpedition.models.explorers.NaturalExplorer;
+import glacialExpedition.models.mission.Mission;
+import glacialExpedition.models.mission.MissionImpl;
+import glacialExpedition.models.states.State;
+import glacialExpedition.models.states.StateImpl;
+import glacialExpedition.repositories.ExplorerRepository;
+import glacialExpedition.repositories.Repository;
+import glacialExpedition.repositories.StateRepository;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static glacialExpedition.common.ConstantMessages.*;
+import static glacialExpedition.common.ExceptionMessages.*;
+
 public class ControllerImpl implements Controller {
+
     private Repository<Explorer> explorerRepository;
     private Repository<State> stateRepository;
-    private int exploredStates;
+    private int exploredStatesCount;
+    private static final double MINIMUM_EXPLORER_ENERGY_NEEDED = 50;
 
     public ControllerImpl() {
         this.explorerRepository = new ExplorerRepository();
@@ -30,7 +32,8 @@ public class ControllerImpl implements Controller {
 
     @Override
     public String addExplorer(String type, String explorerName) {
-        Explorer explorer = null;
+        Explorer explorer;
+
         switch (type) {
             case "AnimalExplorer":
                 explorer = new AnimalExplorer(explorerName);
@@ -42,79 +45,74 @@ public class ControllerImpl implements Controller {
                 explorer = new NaturalExplorer(explorerName);
                 break;
             default:
-                throw new IllegalArgumentException(ExceptionMessages.EXPLORER_INVALID_TYPE);
+                throw new IllegalArgumentException(EXPLORER_INVALID_TYPE);
         }
+
         this.explorerRepository.add(explorer);
-        return String.format(ConstantMessages.EXPLORER_ADDED, type, explorerName);
+        return String.format(EXPLORER_ADDED, type, explorerName);
     }
 
     @Override
     public String addState(String stateName, String... exhibits) {
         State state = new StateImpl(stateName);
 
-        for (String exhibit : exhibits) {
-            state.getExhibits().add(exhibit);
+        for (String exh : exhibits) {
+            state.getExhibits().add(exh);
         }
-        this.stateRepository.add(state);
 
-        return String.format(ConstantMessages.STATE_ADDED, stateName);
+        this.stateRepository.add(state);
+        return String.format(STATE_ADDED, stateName);
     }
 
     @Override
     public String retireExplorer(String explorerName) {
-        Explorer explorer = explorerRepository.byName(explorerName);
+        Explorer explorer = this.explorerRepository.byName(explorerName);
+
         if (explorer == null) {
-            throw new IllegalArgumentException(String.format(ExceptionMessages.EXPLORER_DOES_NOT_EXIST, explorerName));
+            throw new IllegalArgumentException(String.format(EXPLORER_DOES_NOT_EXIST, explorerName));
         }
+
         this.explorerRepository.remove(explorer);
-        return String.format(ConstantMessages.EXPLORER_RETIRED, explorerName);
+        return String.format(EXPLORER_RETIRED, explorerName);
     }
 
     @Override
     public String exploreState(String stateName) {
+        State state = this.stateRepository.byName(stateName);
 
-        List<Explorer> explorers = this.explorerRepository.getCollection().stream()
-                .filter(explorer -> explorer.getEnergy() > 50)
+        List<Explorer> suitableExplorers = this.explorerRepository
+                .getCollection()
+                .stream()
+                .filter(explorer -> explorer.getEnergy() > MINIMUM_EXPLORER_ENERGY_NEEDED)
                 .collect(Collectors.toList());
 
-        if (explorers.isEmpty()) {
-            throw new IllegalArgumentException(ExceptionMessages.STATE_EXPLORERS_DOES_NOT_EXISTS);
+        if (suitableExplorers.isEmpty()) {
+            throw new IllegalArgumentException(STATE_EXPLORERS_DOES_NOT_EXISTS);
         }
 
-        State stateToExplore = this.stateRepository.byName(stateName);
-        
         Mission mission = new MissionImpl();
-        mission.explore(stateToExplore, explorers);
-        
-        long retired = explorers.stream().filter(e -> e.getEnergy() == 0).count();
-        this.exploredStates++;
+        mission.explore(state, suitableExplorers);
 
-        return String.format(ConstantMessages.STATE_EXPLORER, stateName, retired);
+        long retiredExplorers = suitableExplorers.stream().filter(e -> e.getEnergy() == 0).count();
+        this.exploredStatesCount++;
+
+        return String.format(STATE_EXPLORER, stateName, retiredExplorers);
     }
 
     @Override
     public String finalResult() {
         StringBuilder result = new StringBuilder();
-        result.append(String.format(ConstantMessages.FINAL_STATE_EXPLORED, this.exploredStates));
-        result.append(System.lineSeparator());
-        result.append(ConstantMessages.FINAL_EXPLORER_INFO);
 
-        Collection<Explorer> explorers = this.explorerRepository.getCollection();
-       
-        for (Explorer explorer : explorers) {
-            result.append(System.lineSeparator());
-            result.append(String.format(ConstantMessages.FINAL_EXPLORER_NAME, explorer.getName()));
-            result.append(System.lineSeparator());
-            result.append(String.format(ConstantMessages.FINAL_EXPLORER_ENERGY, explorer.getEnergy()));
-            result.append(System.lineSeparator());
-        
-            if(explorer.getSuitcase().getExhibits().isEmpty()){
-                result.append(String.format(ConstantMessages.FINAL_EXPLORER_SUITCASE_EXHIBITS, "None"));
-            } else {
-                result.append(String.format(ConstantMessages.FINAL_EXPLORER_SUITCASE_EXHIBITS,
-                        String.join(ConstantMessages.FINAL_EXPLORER_SUITCASE_EXHIBITS_DELIMITER, explorer.getSuitcase().getExhibits())));
-            }
-        }
-        return result.toString();
+        result.append(String.format(FINAL_STATE_EXPLORED, this.exploredStatesCount))
+                .append(System.lineSeparator());
+        result.append(FINAL_EXPLORER_INFO).append(System.lineSeparator());
+
+        this.explorerRepository
+                .getCollection()
+                .forEach(explorer -> result
+                        .append(explorer.toString())
+                        .append(System.lineSeparator()));
+
+        return result.toString().trim();
     }
 }

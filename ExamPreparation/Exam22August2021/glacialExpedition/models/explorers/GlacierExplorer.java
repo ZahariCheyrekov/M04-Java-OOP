89f5dbc1300e28668e94b614dc21659ec8798d1f
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-25 09:56:10 +0200

    Update GlacierExplorer.java

--- ExamPreparation/Exam22August2021/glacialExpedition/models/explorers/GlacierExplorer.java
+++ ExamPreparation/Exam22August2021/glacialExpedition/models/explorers/GlacierExplorer.java
@@ -1,9 +1,10 @@
-package M04_JavaOOP.ExamPreparation.Exam22August2021.glacialExpedition.models.explorers;
+package glacialExpedition.models.explorers;
 
 public class GlacierExplorer extends BaseExplorer {
-    private static final double GLACIER_EXPLORER_ENERGY = 40;
+
+    private static final double ENERGY = 40;
 
     public GlacierExplorer(String name) {
-        super(name, GLACIER_EXPLORER_ENERGY);
+        super(name, ENERGY);
     }
 }

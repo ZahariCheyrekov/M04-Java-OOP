07a6bb50bf74db32294d767d51c08c96af63dd2f
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-26 11:28:40 +0200

    Update BaseTable.java

--- ExamPreparation/Exam15August2021/restaurant/entities/tables/BaseTable.java
+++ ExamPreparation/Exam15August2021/restaurant/entities/tables/BaseTable.java
@@ -1,18 +1,19 @@
-package M04_JavaOOP.ExamPreparation.Exam15August2021.restaurant.entities.tables;
+package restaurant.entities.tables;
 
-import M04_JavaOOP.ExamPreparation.Exam15August2021.restaurant.entities.drinks.interfaces.Beverages;
-import M04_JavaOOP.ExamPreparation.Exam15August2021.restaurant.entities.healthyFoods.interfaces.HealthyFood;
-import M04_JavaOOP.ExamPreparation.Exam15August2021.restaurant.entities.tables.interfaces.Table;
+import restaurant.common.DataValidator;
+import restaurant.entities.drinks.interfaces.Beverages;
+import restaurant.entities.healthyFoods.interfaces.HealthyFood;
+import restaurant.entities.tables.interfaces.Table;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
 
-import static M04_JavaOOP.ExamPreparation.Exam15August2021.restaurant.common.ExceptionMessages.INVALID_NUMBER_OF_PEOPLE;
-import static M04_JavaOOP.ExamPreparation.Exam15August2021.restaurant.common.ExceptionMessages.INVALID_TABLE_SIZE;
+import static restaurant.common.ExceptionMessages.*;
 
 public abstract class BaseTable implements Table {
-    private List<HealthyFood> healthyFood;
-    private List<Beverages> beverages;
+
+    private Collection<HealthyFood> healthyFood;
+    private Collection<Beverages> beverages;
     private int number;
     private int size;
     private int numberOfPeople;
@@ -21,63 +22,52 @@ public abstract class BaseTable implements Table {
     private double allPeople;
 
     protected BaseTable(int number, int size, double pricePerPerson) {
-        this.setNumber(number);
-        this.setSize(size);
-        this.setPricePerPerson(pricePerPerson);
         this.healthyFood = new ArrayList<>();
         this.beverages = new ArrayList<>();
-    }
-
-    private void setNumber(int number) {
         this.number = number;
+        this.setSize(size);
+        this.pricePerPerson = pricePerPerson;
     }
 
     private void setSize(int size) {
-        if (size <= 0) {
-            throw new IllegalArgumentException(INVALID_TABLE_SIZE);
-        }
+        DataValidator.validateInt(size, INVALID_TABLE_SIZE);
         this.size = size;
-    }
-
-    private void setPricePerPerson(double pricePerPerson) {
-        this.pricePerPerson = pricePerPerson;
     }
 
     @Override
     public int getTableNumber() {
-        return number;
+        return this.number;
     }
 
     @Override
     public int getSize() {
-        return size;
+        return this.size;
     }
 
     @Override
     public int numberOfPeople() {
-        return numberOfPeople;
+        return this.numberOfPeople;
     }
 
     @Override
     public double pricePerPerson() {
-        return pricePerPerson;
+        return this.pricePerPerson;
     }
 
     @Override
     public boolean isReservedTable() {
-        return isReservedTable;
+        return this.isReservedTable;
     }
 
     @Override
     public double allPeople() {
-        return allPeople;
+        return this.allPeople;
     }
 
     @Override
     public void reserve(int numberOfPeople) {
-        if (numberOfPeople <= 0) {
-            throw new IllegalArgumentException(INVALID_NUMBER_OF_PEOPLE);
-        }
+        DataValidator.validateInt(numberOfPeople, INVALID_NUMBER_OF_PEOPLE);
+
         this.numberOfPeople = numberOfPeople;
         this.isReservedTable = true;
     }
@@ -94,33 +84,34 @@ public abstract class BaseTable implements Table {
 
     @Override
     public double bill() {
-        double healthyFoodBill = this.healthyFood
+        double healthyFoodPrice = this.healthyFood
                 .stream()
-                .mapToDouble(HealthyFood::getPrice)
+                .mapToDouble(HealthyFood::getPortion)
                 .sum();
 
-        double beveragesBill = this.beverages
+        double beveragesPrice = this.beverages
                 .stream()
                 .mapToDouble(Beverages::getPrice)
                 .sum();
 
-        return (healthyFoodBill + beveragesBill) + (numberOfPeople * pricePerPerson);
+        return (healthyFoodPrice + beveragesPrice) + this.numberOfPeople * this.pricePerPerson;
     }
 
     @Override
     public void clear() {
-        this.isReservedTable = false;
-        this.numberOfPeople = 0;
         this.healthyFood.clear();
         this.beverages.clear();
+        this.isReservedTable = false;
+        this.numberOfPeople = 0;
     }
 
     @Override
     public String tableInformation() {
-        return String.format("Table - %d%n" +
-                        "Size - %d%n" +
-                        "Type - %s%n" +
+        return String.format("Table - %d" +
+                        "Size - %d" +
+                        "Type - %s" +
                         "All price - %.2f",
-                number, size, this.getClass().getSimpleName(), pricePerPerson);
+                this.number, this.size,
+                this.getClass().getSimpleName(), this.pricePerPerson);
     }
 }

c1980961bd4e0e821f4712ec576fa54cb1d31adc
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-26 11:27:28 +0200

    Update HealthyFood.java

--- ExamPreparation/Exam15August2021/restaurant/entities/healthyFoods/interfaces/HealthyFood.java
+++ ExamPreparation/Exam15August2021/restaurant/entities/healthyFoods/interfaces/HealthyFood.java
@@ -1,4 +1,4 @@
-package M04_JavaOOP.ExamPreparation.Exam15August2021.restaurant.entities.healthyFoods.interfaces;
+package restaurant.entities.healthyFoods.interfaces;
 
 public interface HealthyFood {
     String getName();

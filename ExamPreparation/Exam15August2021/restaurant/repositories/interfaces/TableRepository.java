0842dfb14397386c005d111e6ec0bf350657688d
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-26 11:31:13 +0200

    Update TableRepository.java

--- ExamPreparation/Exam15August2021/restaurant/repositories/interfaces/TableRepository.java
+++ ExamPreparation/Exam15August2021/restaurant/repositories/interfaces/TableRepository.java
@@ -1,4 +1,4 @@
-package M04_JavaOOP.ExamPreparation.Exam15August2021.restaurant.repositories.interfaces;
+package restaurant.repositories.interfaces;
 
 public interface TableRepository<T> extends Repository<T> {
     T byNumber(int number);

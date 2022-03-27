00facaab5ad65a65792d147b24dc63d94bc4c117
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-27 11:33:45 +0300

    Update Biologist.java

--- ExamPreparation/Exam18April2021/spaceStation/models/astronauts/Biologist.java
+++ ExamPreparation/Exam18April2021/spaceStation/models/astronauts/Biologist.java
@@ -3,6 +3,7 @@ package spaceStation.models.astronauts;
 public class Biologist extends BaseAstronaut {
 
     private static final double OXYGEN = 70;
+    private static final double OXYGEN_PER_BREATH = 5;
 
     public Biologist(String name) {
         super(name, OXYGEN);
@@ -10,6 +11,6 @@ public class Biologist extends BaseAstronaut {
 
     @Override
     public void breath() {
-        this.setOxygen(Math.max(0, this.getOxygen() - 5));
+        this.setOxygen(Math.max(0, this.getOxygen()) - OXYGEN_PER_BREATH);
     }
 }

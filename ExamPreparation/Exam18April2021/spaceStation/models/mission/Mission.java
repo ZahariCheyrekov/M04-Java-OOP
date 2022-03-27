79384022397002eb3d91399e001a15f5aac3434e
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-27 11:35:03 +0300

    Update Mission.java

--- ExamPreparation/Exam18April2021/spaceStation/models/mission/Mission.java
+++ ExamPreparation/Exam18April2021/spaceStation/models/mission/Mission.java
@@ -1,12 +1,11 @@
-package M04_JavaOOP.ExamPreparation.Exam18April2021.spaceStation.models.mission;
+package spaceStation.models.mission;
 
-import M04_JavaOOP.ExamPreparation.Exam18April2021.spaceStation.models.astronauts.Astronaut;
-import M04_JavaOOP.ExamPreparation.Exam18April2021.spaceStation.models.planets.Planet;
+import spaceStation.models.astronauts.Astronaut;
+import spaceStation.models.planets.Planet;
 
 import java.util.Collection;
 
 public interface Mission {
     void explore(Planet planet, Collection<Astronaut> astronauts);
-
     int getDeadAstronauts();
 }

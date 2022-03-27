8f83eb218eb018f71348bfc808941fca64d6e764
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-27 12:01:22 +0300

    Update HeroRepositoryTests.java

--- ExamPreparation/Exam18April2021/spaceStation/HeroRepositoryTests.java
+++ ExamPreparation/Exam18April2021/spaceStation/HeroRepositoryTests.java
@@ -5,70 +5,81 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Collection;
-
 public class HeroRepositoryTests {
+
     private HeroRepository heroRepository;
-    private Hero hero;
+    private Hero hulk;
+
+    private static final String HERO_NAME_NULL = null;
+    private static final String HERO_NAME_HULK = "Hulk";
+    private static final String HERO_NAME_BATMAN = "Batman";
+
+    private static final int HERO_LEVEL_HUNDRED = 100;
+    private static final int HERO_LEVEL_FIFTY = 50;
+
+    private static final int HERO_COUNT_TWO = 2;
+
+    private static final Hero BATMAN = new Hero(HERO_NAME_BATMAN, HERO_LEVEL_FIFTY);
+    private static final Hero NULL_HERO = null;
+    private static final Hero HERO_WITH_EXISTING_NAME = new Hero(HERO_NAME_HULK, HERO_LEVEL_FIFTY);
+
+    private static final String SUCCESSFULLY_CREATED_HERO = "Successfully added hero %s with level %d";
 
     @Before
     public void setUp() {
-        this.heroRepository = new HeroRepository();
-        this.hero = new Hero("Superman", 100);
-        this.heroRepository.create(hero);
-    }
-
-    @Test
-    public void testShouldAddHeroToRepositoryAndGetCorrectCount() {
-        int expectedHeroesCount = 1;
-        int actualHeroesCount = this.heroRepository.getCount();
-        assertEquals(expectedHeroesCount, actualHeroesCount);
+        heroRepository = new HeroRepository();
+        hulk = new Hero(HERO_NAME_HULK, HERO_LEVEL_HUNDRED);
+        heroRepository.create(hulk);
     }
 
     @Test(expected = NullPointerException.class)
-    public void testShouldFailForNullHero() {
-        this.heroRepository.create(null);
+    public void testShouldThrowExceptionFoTryingToCreateHeroWithValueNull() {
+        heroRepository.create(NULL_HERO);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testShouldThrowExceptionForExistingHero() {
-        this.heroRepository.create(hero);
-    }
-
-    @Test(expected = NullPointerException.class)
-    public void testShouldFailForNullNameInRemoveMethod() {
-        this.heroRepository.remove(null);
+    public void testShouldThrowExceptionForAlreadyExistingHeroWithThatName() {
+        heroRepository.create(HERO_WITH_EXISTING_NAME);
     }
 
     @Test
-    public void testShouldRemoveCorrectHeroWithGivenName() {
-        boolean isRemoved = this.heroRepository.remove("Superman");
+    public void testShouldCreateHeroCorrectlyAndReturnTheInformationAboutCreation() {
+        String expectedMessage = String.format(SUCCESSFULLY_CREATED_HERO, HERO_NAME_BATMAN, HERO_LEVEL_FIFTY);
+        String actualMessage = heroRepository.create(BATMAN);
+
+        assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testShouldThrowExceptionForNameWithValueNull() {
+        heroRepository.remove(HERO_NAME_NULL);
+    }
+
+    @Test
+    public void testShouldRemoveHeroFromTheRepositoryByTheGivenName() {
+        boolean isRemoved = heroRepository.remove(HERO_NAME_HULK);
         assertTrue(isRemoved);
     }
 
     @Test
-    public void testShouldReturnTheHeroWithTheHighestLevel() {
-        this.heroRepository.create(new Hero("Iron Man", 99));
-        Hero expected = this.hero;
-        Hero actual = this.heroRepository.getHeroWithHighestLevel();
-        assertEquals(expected, actual);
+    public void testShouldGetTheHeroWithTheHighestLevel() {
+        heroRepository.create(BATMAN);
+
+        int actualHeroCount = heroRepository.getCount();
+        assertEquals(HERO_COUNT_TWO, actualHeroCount);
+
+        Hero heroWithHighestLevel = heroRepository.getHeroWithHighestLevel();
+        assertEquals(hulk, heroWithHighestLevel);
     }
 
     @Test
-    public void testShouldGetCorrectHeroWithGivenName() {
-        Hero expected = this.hero;
-        Hero actual = this.heroRepository.getHero("Superman");
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void testShouldReturnCollectionOfHeroes() {
-        Collection<Hero> heroes = this.heroRepository.getHeroes();
-        assertEquals(1, heroes.size());
+    public void testShouldGetHeroByTheGivenNameCorrectly() {
+        Hero heroByName = heroRepository.getHero(HERO_NAME_HULK);
+        assertEquals(hulk, heroByName);
     }
 
     @Test(expected = UnsupportedOperationException.class)
-    public void testShouldThrowExceptionForTryingToChangeUnmodifiableCollection() {
-        this.heroRepository.getHeroes().clear();
+    public void testShouldThrowExceptionForTryingToModifyUnmodifiableCollection() {
+        heroRepository.getHeroes().clear();
     }
 }

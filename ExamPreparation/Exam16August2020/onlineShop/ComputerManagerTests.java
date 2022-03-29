0021333944c4c88e2c6815f2e077c8b011d4049b
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-29 11:10:52 +0300

    Update ComputerManagerTests.java

--- ExamPreparation/Exam16August2020/onlineShop/ComputerManagerTests.java
+++ ExamPreparation/Exam16August2020/onlineShop/ComputerManagerTests.java
@@ -3,9 +3,6 @@ package computers;
 public class ComputerManagerTests {
 
 
-
-    @Test
-    public void testShouldGetComputersByGivenManufacturer() {
         List<Computer> computersByManufacturer = computerManager.getComputersByManufacturer("Acer");
         assertEquals(computer, computersByManufacturer.get(0));
     }

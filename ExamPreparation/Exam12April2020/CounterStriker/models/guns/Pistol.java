53db61706fbb2ae2d4da0d842cef6809442011bf
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-30 11:31:15 +0300

    Update Pistol.java

--- ExamPreparation/Exam12April2020/CounterStriker/models/guns/Pistol.java
+++ ExamPreparation/Exam12April2020/CounterStriker/models/guns/Pistol.java
@@ -2,18 +2,7 @@ package CounterStriker.models.guns;
 
 public class Pistol extends GunImpl {
 
-    private static final int BULLETS_TO_FIRE = 1;
-
     public Pistol(String name, int bulletsCount) {
         super(name, bulletsCount);
-    }
-
-    @Override
-    public int fire() {
-        if (super.getBulletsCount() < BULLETS_TO_FIRE) {
-            return 0;
-        }
-        super.decreaseBullets(BULLETS_TO_FIRE);
-        return BULLETS_TO_FIRE;
     }
 }

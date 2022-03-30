55724cbbebbddb0df62a0d6599c66003301b5d88
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-30 11:29:18 +0300

    Update OutputMessages.java

--- ExamPreparation/Exam12April2020/CounterStriker/common/OutputMessages.java
+++ ExamPreparation/Exam12April2020/CounterStriker/common/OutputMessages.java
@@ -1,6 +1,7 @@
-package M04_JavaOOP.ExamPreparation.Exam12April2020.CounterStriker.common;
+package CounterStriker.common;
 
 public class OutputMessages {
+
     public static final String SUCCESSFULLY_ADDED_PLAYER = "Successfully added player %s.";
 
     public static final String SUCCESSFULLY_ADDED_GUN = "Successfully added gun %s.";
@@ -8,4 +9,12 @@ public class OutputMessages {
     public static final String TERRORIST_WINS = "Terrorist wins!";
 
     public static final String COUNTER_TERRORIST_WINS = "Counter Terrorist wins!";
+
+    public static final String PLAYER_TO_STRING = "%s: %s";
+
+    public static final String PLAYER_HEALTH_TO_STRING = "--Health: %d";
+
+    public static final String PLAYER_ARMOR_TO_STRING = "--Armor: %d";
+
+    public static final String PLAYER_GUN_TO_STRING = "--Gun: %s";
 }

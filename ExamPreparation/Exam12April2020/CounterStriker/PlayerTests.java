beb49587a69a2ee9d91d95daaa2c6457cd325c7f
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-30 11:56:40 +0300

    Update PlayerTests.java

--- ExamPreparation/Exam12April2020/CounterStriker/PlayerTests.java
+++ ExamPreparation/Exam12April2020/CounterStriker/PlayerTests.java
@@ -1,41 +1,73 @@
 package halfLife;
 
+import static org.junit.Assert.*;
+
 import org.junit.Before;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 public class PlayerTests {
 
     private Player player;
-    private final Player DEAD_PLAYER = new Player("dead", 0);
     private Gun gun;
+
+    private static final String PLAYER_USERNAME = "Neo";
+    private static final int PLAYER_HEALTH = 100;
+
+    private static final String GUN_NAME = "Rifle";
+    private static final int GUN_BULLETS = 50;
+
+    private static final Gun NULL_GUN = null;
+    private static final String NULL_NAME = null;
+    private static final int INVALID_HEALTH = -100;
+
+    private static final int FIFTY_DAMAGE = 50;
+    private static final int HUNDRED_DAMAGE = 100;
+    private static final int TWO_HUNDRED_DAMAGE = 200;
+
+    private static final int ZERO_HEALTH = 0;
+    private static final int FIFTY_HEALTH = 50;
 
     @Before
     public void setUp() {
-        player = new Player("Glory", 100);
-        gun = new Gun("Rifle", 100);
+        player = new Player(PLAYER_USERNAME, PLAYER_HEALTH);
+        gun = new Gun(GUN_NAME, GUN_BULLETS);
         player.addGun(gun);
     }
 
     @Test(expected = NullPointerException.class)
-    public void testShouldThrowExceptionForNullName() {
-        new Player(null, 100);
+    public void testShouldThrowExceptionForTryingToSetNullNameToPlayer() {
+        new Player(NULL_NAME, PLAYER_HEALTH);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testShouldThrowExceptionForHealthBelowZero() {
-        new Player("invalid", -10);
+    public void testShouldThrowExceptionForTryingToSetInvalidHealthToPlayer() {
+        new Player(PLAYER_USERNAME, INVALID_HEALTH);
     }
 
     @Test(expected = UnsupportedOperationException.class)
-    public void testShouldThrowExceptionWhenTryingToModifyGunsCollection() {
+    public void testShouldThrowExceptionForTryingToModifyUnmodifiableCollection() {
         player.getGuns().clear();
     }
 
+    @Test(expected = IllegalStateException.class)
+    public void testShouldThrowExceptionWhenTryingToTakeDamageFromAlreadyDeadPlayer() {
+        player.takeDamage(HUNDRED_DAMAGE);
+        player.takeDamage(HUNDRED_DAMAGE);
+    }
+
+    @Test
+    public void testShouldTakeDamageFromPlayerCorrectly() {
+        player.takeDamage(FIFTY_DAMAGE);
+        assertEquals(FIFTY_HEALTH, player.getHealth());
+
+        player.takeDamage(TWO_HUNDRED_DAMAGE);
+        int actualHealth = player.getHealth();
+        assertEquals(ZERO_HEALTH, actualHealth);
+    }
+
     @Test(expected = NullPointerException.class)
-    public void testShouldThrowExceptionWhenTryingToAddNullGun() {
-        player.addGun(null);
+    public void testShouldThrowExceptionForTryingToAddNullGunToPlayer() {
+        player.addGun(NULL_GUN);
     }
 
     @Test
@@ -45,32 +77,14 @@ public class PlayerTests {
     }
 
     @Test
-    public void testShouldReturnGunByGivenName() {
-        Gun gunByName = player.getGun(this.gun.getName());
+    public void testShouldGetGunByGivenNameFromPlayerGunsCollection() {
+        Gun gunByName = player.getGun(GUN_NAME);
         assertEquals(gun, gunByName);
     }
 
-    @Test(expected = IllegalStateException.class)
-    public void testShouldThrowExceptionForAlreadyDeadPlayer() {
-        DEAD_PLAYER.takeDamage(10);
-    }
-
     @Test
-    public void testShouldTakeDamageFromPlayerCorrectly() {
-        player.takeDamage(10);
-        int expectedHealth = 90;
-        int actualHealth = player.getHealth();
-        assertEquals(expectedHealth, actualHealth);
-
-        player.takeDamage(100);
-        boolean zeroHealth = player.getHealth() == 0;
-        assertTrue(zeroHealth);
-    }
-
-    @Test
-    public void testShouldGetUsernameCorrectly() {
-        String expected = "Glory";
-        String actual = player.getUsername();
-        assertEquals(expected, actual);
+    public void testShouldReturnPlayerUsernameCorrectly() {
+        String username = player.getUsername();
+        assertEquals(PLAYER_USERNAME, username);
     }
 }

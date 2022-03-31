6067d422a088fda3d53f8e31d7d61b186ab4dbba
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-31 10:46:47 +0300

    Update SpaceshipTests.java

--- ExamPreparation/Exam19Dec2020/viceCity/SpaceshipTests.java
+++ ExamPreparation/Exam19Dec2020/viceCity/SpaceshipTests.java
@@ -8,19 +8,6 @@ public class SpaceshipTests {
 
     
 
-    @Test(expected = IllegalArgumentException.class)
-    public void testShouldThrowExceptionForAlreadyExistingAstronautOnTheSpaceship() {
-        spaceship.add(astronaut);
-        spaceship.add(astronaut);
-    }
-   
-    @Test
-    public void testShouldGetSpaceshipNameCorrectly() {
-        String expectedName = "FlagonXII";
-        String actualName = spaceship.getName();
-        assertEquals(expectedName, actualName);
-    }
-    
     @Test
     public void testShouldGetSpaceshipAstronautsCountCorrectly() {
         spaceship.add(astronaut);

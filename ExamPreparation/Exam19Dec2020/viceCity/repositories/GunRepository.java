c245615d0e84485ddb2a1dc9edfc941ebc5395a1
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-31 10:28:07 +0300

    Update GunRepository.java

--- ExamPreparation/Exam19Dec2020/viceCity/repositories/GunRepository.java
+++ ExamPreparation/Exam19Dec2020/viceCity/repositories/GunRepository.java
@@ -1,34 +1,38 @@
-package viceCity.repositories.interfaces;
+package viceCity.repositories;
 
 import viceCity.models.guns.Gun;
+import viceCity.repositories.interfaces.Repository;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class GunRepository implements Repository<Gun> {
 
-    private Map<String, Gun> models;
+    private final Map<String, Gun> guns;
 
     public GunRepository() {
-        this.models = new LinkedHashMap<>();
+        this.guns = new LinkedHashMap<>();
     }
 
     @Override
     public Collection<Gun> getModels() {
-        return Collections.unmodifiableCollection(this.models.values());
+        return Collections.unmodifiableCollection(this.guns.values());
     }
 
     @Override
     public void add(Gun model) {
-        this.models.putIfAbsent(model.getName(), model);
+        this.guns.putIfAbsent(model.getName(), model);
     }
 
     @Override
     public boolean remove(Gun model) {
-        return this.models.remove(model.getName()) != null;
+        return this.guns.remove(model.getName()) != null;
     }
 
     @Override
     public Gun find(String name) {
-        return this.models.get(name);
+        return this.guns.get(name);
     }
 }

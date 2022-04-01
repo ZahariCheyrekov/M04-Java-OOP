f615c971b9795dbc01f66eb986ade1b89454b47d
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-04-01 10:43:53 +0300

    Update ConsoleWriter.java

--- ExamPreparation/Exam20December2021/christmasRaces/io/ConsoleWriter.java
+++ ExamPreparation/Exam20December2021/christmasRaces/io/ConsoleWriter.java
@@ -3,6 +3,7 @@ package christmasRaces.io;
 import christmasRaces.io.interfaces.OutputWriter;
 
 public class ConsoleWriter implements OutputWriter {
+ 
     @Override
     public void writeLine(String text) {
         System.out.println(text);

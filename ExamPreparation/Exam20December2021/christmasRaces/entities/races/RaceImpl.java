0b4c5f7c78427720533087b855534a917cfd2373
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-04-01 10:42:56 +0300

    Update RaceImpl.java

--- ExamPreparation/Exam20December2021/christmasRaces/entities/races/RaceImpl.java
+++ ExamPreparation/Exam20December2021/christmasRaces/entities/races/RaceImpl.java
@@ -1,9 +1,9 @@
 package christmasRaces.entities.races;
 
+import christmasRaces.common.DataValidator;
 import christmasRaces.entities.drivers.Driver;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.*;
 
 import static christmasRaces.common.ExceptionMessages.*;
 
@@ -11,25 +11,23 @@ public class RaceImpl implements Race {
 
     private String name;
     private int laps;
-    private Collection<Driver> drivers;
+    private Map<String, Driver> drivers;
 
     public RaceImpl(String name, int laps) {
         this.setName(name);
         this.setLaps(laps);
-        this.drivers = new ArrayList<>();
+        this.drivers = new LinkedHashMap<>();
     }
 
     private void setName(String name) {
-        if (name == null || name.trim().isEmpty()) {
-            throw new IllegalArgumentException(String.format(INVALID_NAME, name, 5));
-        }
+        String exceptionMessage = String.format(INVALID_NAME, name, 5);
+        DataValidator.validateString(name, exceptionMessage, 5);
         this.name = name;
     }
 
     private void setLaps(int laps) {
-        if (laps < 1) {
-            throw new IllegalArgumentException(INVALID_NUMBER_OF_LAPS);
-        }
+        String exceptionMessage = String.format(INVALID_NUMBER_OF_LAPS, 1);
+        DataValidator.validateLaps(laps, 1, exceptionMessage);
         this.laps = laps;
     }
 
@@ -45,7 +43,7 @@ public class RaceImpl implements Race {
 
     @Override
     public Collection<Driver> getDrivers() {
-        return this.drivers;
+        return this.drivers.values();
     }
 
     @Override
@@ -54,22 +52,18 @@ public class RaceImpl implements Race {
             throw new IllegalArgumentException(DRIVER_INVALID);
         }
 
-        boolean canParticipate = driver.getCanParticipate();
-        if (!canParticipate) {
-            throw new IllegalArgumentException(String.format(DRIVER_NOT_PARTICIPATE, driver.getName()));
+        if (!driver.getCanParticipate()) {
+            String exceptionMessage = String.format(DRIVER_NOT_PARTICIPATE, driver.getName());
+            throw new IllegalArgumentException(exceptionMessage);
         }
 
-        Driver existingDriver = this.drivers
-                .stream()
-                .filter(d -> d.getName()
-                        .equals(driver.getName()))
-                .findFirst()
-                .orElse(null);
+        boolean isExisting = this.drivers.get(driver.getName()) != null;
 
-        if (existingDriver != null) {
-            throw new IllegalArgumentException(String.format(DRIVER_ALREADY_ADDED, driver.getName(), this.name));
+        if (isExisting) {
+            String exceptionMessage = String.format(DRIVER_ALREADY_ADDED, driver.getName(), this.name);
+            throw new IllegalArgumentException(exceptionMessage);
         }
 
-        this.drivers.add(driver);
+        this.drivers.put(driver.getName(), driver);
     }
 }

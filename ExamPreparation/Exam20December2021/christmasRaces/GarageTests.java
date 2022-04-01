a5bffef9dee85d18feda06edd3e7230951d72827
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-04-01 11:00:59 +0300

    Update GarageTests.java

--- ExamPreparation/Exam20December2021/christmasRaces/GarageTests.java
+++ ExamPreparation/Exam20December2021/christmasRaces/GarageTests.java
@@ -2,15 +2,4 @@ package garage;
 
 import static org.junit.Assert.*;
 
-import org.junit.Before;
-import org.junit.Test;
-ngage.getTheMostExpensiveCar();
-        assertEquals(lamborghini, mostExpensiveCar);
-    }
-
-    @Test
-    public void testShouldGetAllCarsFromTheGarageByTheGivenBrand() {
-        List<Car> allCarsByBrand = garage.findAllCarsByBrand(CAR_BRAND);
-        assertEquals(lamborghini, allCarsByBrand.get(INDEX));
-    }
 }

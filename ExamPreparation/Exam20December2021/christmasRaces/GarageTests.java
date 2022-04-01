3f3bf8aca719c67ba409a8a6327b9126b8ea5203
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-04-01 11:00:53 +0300

    Update GarageTests.java

--- ExamPreparation/Exam20December2021/christmasRaces/GarageTests.java
+++ ExamPreparation/Exam20December2021/christmasRaces/GarageTests.java
@@ -4,20 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
-ng CAR_BRAND = "Lamborghini";
-    private static final int SPEED = 500;
-    private static final int CARS_IN_GARAGE = 2;
-    private static final int INDEX = 0;
-
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testShouldThrowExceptionForTryingToAddNullCarInTheGarage() {
-        garage.addCar(NULL_CAR);
-    }
-
-    @Test
-    public void testShouldGetMostExpensiveCarFromTheGarage() {
-        Car mostExpensiveCar = garage.getTheMostExpensiveCar();
+ngage.getTheMostExpensiveCar();
         assertEquals(lamborghini, mostExpensiveCar);
     }
 

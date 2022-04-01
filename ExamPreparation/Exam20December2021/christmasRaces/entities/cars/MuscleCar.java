aa6e9f33a7f86a05099da626f1c7deeae4978f50
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-04-01 10:41:45 +0300

    Update MuscleCar.java

--- ExamPreparation/Exam20December2021/christmasRaces/entities/cars/MuscleCar.java
+++ ExamPreparation/Exam20December2021/christmasRaces/entities/cars/MuscleCar.java
@@ -1,12 +1,15 @@
 package christmasRaces.entities.cars;
 
+import christmasRaces.common.DataValidator;
+
 import static christmasRaces.common.ExceptionMessages.INVALID_HORSE_POWER;
 
 public class MuscleCar extends BaseCar {
 
-    private static final double CUBIC_CENTIMETERS = 5000;
     private static final int MINIMUM_HORSEPOWER = 400;
     private static final int MAXIMUM_HORSEPOWER = 600;
+
+    private static final double CUBIC_CENTIMETERS = 5000;
 
     public MuscleCar(String model, int horsePower) {
         super(model, horsePower, CUBIC_CENTIMETERS);
@@ -14,9 +17,8 @@ public class MuscleCar extends BaseCar {
 
     @Override
     protected void setHorsePower(int horsePower) {
-        if (horsePower < MINIMUM_HORSEPOWER || horsePower > MAXIMUM_HORSEPOWER) {
-            throw new IllegalArgumentException(String.format(INVALID_HORSE_POWER, horsePower));
-        }
+        String exceptionMessage = String.format(INVALID_HORSE_POWER, horsePower);
+        DataValidator.validateHorsepower(horsePower, MINIMUM_HORSEPOWER, MAXIMUM_HORSEPOWER, exceptionMessage);
         super.setHorsePower(horsePower);
     }
 }

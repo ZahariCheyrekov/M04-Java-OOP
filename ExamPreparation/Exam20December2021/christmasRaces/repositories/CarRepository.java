41790fab6b254b1781178038130ec581232486e2
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-04-01 10:44:29 +0300

    Update CarRepository.java

--- ExamPreparation/Exam20December2021/christmasRaces/repositories/CarRepository.java
+++ ExamPreparation/Exam20December2021/christmasRaces/repositories/CarRepository.java
@@ -3,7 +3,10 @@ package christmasRaces.repositories;
 import christmasRaces.entities.cars.Car;
 import christmasRaces.repositories.interfaces.Repository;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class CarRepository implements Repository<Car> {
 

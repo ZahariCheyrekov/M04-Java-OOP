c815be56c2a2b93b0de4729efc37cc00e34e7528
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-04-02 10:08:24 +0300

    Update LonghairCat.java

--- ExamPreparation/Exam11December2021/catHouse/entities/cat/LonghairCat.java
+++ ExamPreparation/Exam11December2021/catHouse/entities/cat/LonghairCat.java
@@ -3,7 +3,7 @@ package catHouse.entities.cat;
 public class LonghairCat extends BaseCat {
 
     private static final int KILOGRAMS = 9;
-    private static final int INCREASE_PER_EAT = 3;
+    private static final int INCREASE_WEIGHT_PER_MEAL = 3;
 
     public LonghairCat(String name, String breed, double price) {
         super(name, breed, price);
@@ -11,6 +11,6 @@ public class LonghairCat extends BaseCat {
 
     @Override
     public void eating() {
-        this.setKilograms(KILOGRAMS + INCREASE_PER_EAT);
+        this.setKilograms(KILOGRAMS + INCREASE_WEIGHT_PER_MEAL);
     }
 }

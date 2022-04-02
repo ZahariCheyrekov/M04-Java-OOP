a03f77c489f7fdcdf031dce7d7ade11286c1d745
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-04-02 10:06:23 +0300

    Update ConstantMessages.java

--- ExamPreparation/Exam11December2021/catHouse/common/ConstantMessages.java
+++ ExamPreparation/Exam11December2021/catHouse/common/ConstantMessages.java
@@ -1,6 +1,7 @@
 package catHouse.common;
 
 public class ConstantMessages {
+
     public static final String NOT_ENOUGH_CAPACITY_FOR_CAT = "Not enough capacity for this cat.";
     public static final String UNSUITABLE_HOUSE = "Unsuitable house.";
     public static final String SUCCESSFULLY_ADDED_CAT_IN_HOUSE = "Successfully added %s to %s.";
@@ -9,4 +10,9 @@ public class ConstantMessages {
     public static final String SUCCESSFULLY_ADDED_HOUSE_TYPE = "%s is successfully added.";
     public static final String SUCCESSFULLY_ADDED_TOY_TYPE = "%s is successfully added.";
     public static final String VALUE_HOUSE = "The value of House %s is %.2f.";
+    public static final String NONE_CATS_IN_THE_HOUSE = "none";
+    public static final String CATS_NAMES = "Cats: %s";
+    public static final String CAT_HOUSE_TO_STRING = "%s %s:";
+    public static final String CATS_NAMES_DELIMITER = " ";
+    public static final String TOYS_COUNT_ADN_SOFTNESS_TO_STRING = "Toys: %d Softness: %d";
 }

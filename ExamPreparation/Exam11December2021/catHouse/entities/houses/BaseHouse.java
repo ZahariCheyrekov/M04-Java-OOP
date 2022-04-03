90aadea9475f88544c383b774db790be5879fef6
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-04-03 19:11:32 +0300

    Update BaseHouse.java

--- ExamPreparation/Exam11December2021/catHouse/entities/houses/BaseHouse.java
+++ ExamPreparation/Exam11December2021/catHouse/entities/houses/BaseHouse.java
@@ -1,11 +1,6 @@
 package catHouse.entities.houses;
 
     @Override
-    public Collection<Cat> getCats() {
-        return this.cats;
-    }
-
-    @Override
     public Collection<Toy> getToys() {
         return this.toys;
     }

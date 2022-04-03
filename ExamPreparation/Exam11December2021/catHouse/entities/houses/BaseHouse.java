242a810631cc421df97428d53a84eac58bd00a67
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-04-03 19:11:20 +0300

    Update BaseHouse.java

--- ExamPreparation/Exam11December2021/catHouse/entities/houses/BaseHouse.java
+++ ExamPreparation/Exam11December2021/catHouse/entities/houses/BaseHouse.java
@@ -1,12 +1,5 @@
 package catHouse.entities.houses;
 
-        int toysCount = toys.size();
-        int sumOfSoftness = toys.stream().mapToInt(Toy::getSoftness).sum();
-
-        stats.append(String.format(TOYS_COUNT_ADN_SOFTNESS_TO_STRING, toysCount, sumOfSoftness))
-                .append(System.lineSeparator());
-
-        return stats.toString().trim();
     }
 
     @Override

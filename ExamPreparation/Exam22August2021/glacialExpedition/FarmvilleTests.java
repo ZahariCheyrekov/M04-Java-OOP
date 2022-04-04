4039c967792c10faa21100a660b0186573c73098
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-04-04 20:47:53 +0300

    Update FarmvilleTests.java

--- ExamPreparation/Exam22August2021/glacialExpedition/FarmvilleTests.java
+++ ExamPreparation/Exam22August2021/glacialExpedition/FarmvilleTests.java
@@ -3,5 +3,14 @@ import org.junit.Test;
 
 public class FarmvilleTests {
 
+  private Farm farm;
+  private Animal dog;
+
+  private static final String FARM_NAME = "HappyFarm";
+  private static final String NULL_NAME = null;
+  private static final int FARM_CAPACITY = 2;
+  private static final int INVALID_CAPACITY = -10;
+  private static final int FARM_ANIMALS_COUNT = 1;
+  
   
 }

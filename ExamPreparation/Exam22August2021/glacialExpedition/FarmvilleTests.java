3852c9d83e8ea27ee01a85c515f64c831c2819ff
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-04-04 20:45:23 +0300

    Update FarmvilleTests.java

--- ExamPreparation/Exam22August2021/glacialExpedition/FarmvilleTests.java
+++ ExamPreparation/Exam22August2021/glacialExpedition/FarmvilleTests.java
@@ -5,12 +5,6 @@ package farmville;
 public class FarmvilleTests {
 
 
-    private static final String FARM_NAME = "HappyFarm";
-    private static final String NULL_NAME = null;
-    private static final int FARM_CAPACITY = 2;
-    private static final int INVALID_CAPACITY = -10;
-    private static final int FARM_ANIMALS_COUNT = 1;
-
     private static final String DOG_TYPE = "Dog";
     private static final String COW_TYPE = "Cow";
     private static final String SHEEP_TYPE = "Sheep";

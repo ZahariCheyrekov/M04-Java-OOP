24b65bb5863988a95bbede719ab6166805748d78
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-04-04 20:46:23 +0300

    Update FarmvilleTests.java

--- ExamPreparation/Exam22August2021/glacialExpedition/FarmvilleTests.java
+++ ExamPreparation/Exam22August2021/glacialExpedition/FarmvilleTests.java
@@ -5,8 +5,6 @@ package farmville;
 public class FarmvilleTests {
 
 
-    @Test
-    public void testShouldGetFarmNameCorrectly() {
         String actualName = farm.getName();
         assertEquals(FARM_NAME, actualName);
     }

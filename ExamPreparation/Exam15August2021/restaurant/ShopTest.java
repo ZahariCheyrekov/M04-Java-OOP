7e24126b0498e18a133f2b9ef9fd900d7cb0f8c8
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-04-05 21:21:46 +0300

    Update ShopTest.java

--- ExamPreparation/Exam15August2021/restaurant/ShopTest.java
+++ ExamPreparation/Exam15August2021/restaurant/ShopTest.java
@@ -5,12 +5,4 @@ public class ShopTest {
 
 
 
-    public void testRemoveGoodsShouldSetTheShelveValueToNull() throws OperationNotSupportedException {
-        Goods goods = new Goods("testGood", "testCode");
-        shop.addGoods("Shelves1", goods);
-        shop.removeGoods("Shelves1", goods);
-
-        Goods emptySlot = shop.getShelves().get("Shelves1");
-        assertNull(emptySlot);
-    }
 }

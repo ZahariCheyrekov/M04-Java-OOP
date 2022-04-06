7efe2826fd38296191df52fe16ed63cec3ef3452
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-04-06 18:31:51 +0300

    Update HeroRepositoryTests.java

--- ExamPreparation/Exam18April2021/spaceStation/HeroRepositoryTests.java
+++ ExamPreparation/Exam18April2021/spaceStation/HeroRepositoryTests.java
@@ -1,7 +1,6 @@
 package heroRepository;
 
 public class HeroRepositoryTests {
-    @Test
     public void testShouldRemoveHeroFromTheRepositoryByTheGivenName() {
         boolean isRemoved = heroRepository.remove(HERO_NAME_HULK);
         assertTrue(isRemoved);

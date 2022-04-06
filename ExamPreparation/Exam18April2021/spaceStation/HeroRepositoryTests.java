48793beaf254d8c1f6aae6555da615e9a597ac8f
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-04-06 18:32:17 +0300

    Update HeroRepositoryTests.java

--- ExamPreparation/Exam18April2021/spaceStation/HeroRepositoryTests.java
+++ ExamPreparation/Exam18April2021/spaceStation/HeroRepositoryTests.java
@@ -5,11 +5,7 @@ public class HeroRepositoryTests {
     public void testShouldGetTheHeroWithTheHighestLevel() {
     }
 
-    @Test
-    public void testShouldGetHeroByTheGivenNameCorrectly() {
-        Hero heroByName = heroRepository.getHero(HERO_NAME_HULK);
-        assertEquals(hulk, heroByName);
-    }
+  
 
     @Test(expected = UnsupportedOperationException.class)
     public void testShouldThrowExceptionForTryingToModifyUnmodifiableCollection() {

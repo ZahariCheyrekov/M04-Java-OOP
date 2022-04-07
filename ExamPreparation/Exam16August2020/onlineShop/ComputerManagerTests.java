83a584dec0bdaa753441ab40c0d7ea6e0a0b4093
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-04-07 15:38:14 +0300

    Update ComputerManagerTests.java

--- ExamPreparation/Exam16August2020/onlineShop/ComputerManagerTests.java
+++ ExamPreparation/Exam16August2020/onlineShop/ComputerManagerTests.java
@@ -1,12 +1,3 @@
 public class ComputerManagerTests {
-    public void testShouldGetComputerByManufacturerCorrectly() {
-        List<Computer> computersByManufacturer = computerManager.getComputersByManufacturer(COMPUTER_MANUFACTURER);
-        assertEquals(computer, computersByManufacturer.get(COMPUTER_INDEX));
-    }
-
-    @Test
-    public void testShouldGetComputersCountFromTheComputerManager() {
-        int computerManagerCount = computerManager.getCount();
-        assertEquals(COMPUTERS_COUNT, computerManagerCount);
-    }
+    
 }

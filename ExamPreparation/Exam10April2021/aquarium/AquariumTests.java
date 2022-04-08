aae9ff15840a8f64be9c0ecf1c965d5495ad8a59
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-04-08 20:27:14 +0300

    Update AquariumTests.java

--- ExamPreparation/Exam10April2021/aquarium/AquariumTests.java
+++ ExamPreparation/Exam10April2021/aquarium/AquariumTests.java
@@ -2,12 +2,6 @@
 
 
 public class AquariumTests {
-    public void testShouldSellFishFromTheAquariumByTheGivenName() {
-        Fish soldFish = aquarium.sellFish(FISH_NAME);
-        assertEquals(fish, soldFish);
-        assertFalse(soldFish.isAvailable());
-    }
-
     @Test
     public void testShouldReturnTheReportForTheFishNamesInTheAquarium() {
         String expectedReport = String.format(REPORT_MESSAGE, aquarium.getName(), FISH_NAME);

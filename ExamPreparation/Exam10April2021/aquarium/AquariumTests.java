62ab749f07543ee116fff1e5a5babb4c92187bf4
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-04-08 20:27:37 +0300

    Update AquariumTests.java

--- ExamPreparation/Exam10April2021/aquarium/AquariumTests.java
+++ ExamPreparation/Exam10April2021/aquarium/AquariumTests.java
@@ -2,9 +2,6 @@
 
 
 public class AquariumTests {
-    @Test
     public void testShouldGetTheAquariumCapacityCorrectly() {
-        int actualCapacity = aquarium.getCapacity();
-        assertEquals(AQUARIUM_CAPACITY, actualCapacity);
     }
 }

e25384f71d57b0f2e89ff188743756e7c0f3ba20
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-04-10 21:55:53 +0300

    Update PlayerTests.java

--- ExamPreparation/Exam12April2020/CounterStriker/PlayerTests.java
+++ ExamPreparation/Exam12April2020/CounterStriker/PlayerTests.java
@@ -3,27 +3,4 @@
 
 public class PlayerTests {
 
-
-    @Test(expected = NullPointerException.class)
-    public void testShouldThrowExceptionForTryingToAddNullGunToPlayer() {
-        player.addGun(NULL_GUN);
-    }
-
-    @Test
-    public void testShouldRemoveGunFromPlayerCorrectly() {
-        boolean isRemoved = player.removeGun(gun);
-        assertTrue(isRemoved);
-    }
-
-    @Test
-    public void testShouldGetGunByGivenNameFromPlayerGunsCollection() {
-        Gun gunByName = player.getGun(GUN_NAME);
-        assertEquals(gun, gunByName);
-    }
-
-    @Test
-    public void testShouldReturnPlayerUsernameCorrectly() {
-        String username = player.getUsername();
-        assertEquals(PLAYER_USERNAME, username);
-    }
 }

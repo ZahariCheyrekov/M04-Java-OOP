2ec4342b81a2b9588117eca11a7bc7b6a4d36751
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-04-10 21:55:22 +0300

    Update PlayerTests.java

--- ExamPreparation/Exam12April2020/CounterStriker/PlayerTests.java
+++ ExamPreparation/Exam12April2020/CounterStriker/PlayerTests.java
@@ -2,8 +2,6 @@
 
 
 public class PlayerTests {
-    public void testShouldThrowExceptionWhenTryingToTakeDamageFromAlreadyDeadPlayer() {
-    }
 
     @Test
     public void testShouldTakeDamageFromPlayerCorrectly() {

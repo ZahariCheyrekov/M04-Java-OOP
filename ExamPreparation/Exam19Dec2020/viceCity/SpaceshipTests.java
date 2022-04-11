0ee216f8e3b8a3ae6884faedb0c9f60adb5bd32b
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-04-11 22:05:39 +0300

    Update SpaceshipTests.java

--- ExamPreparation/Exam19Dec2020/viceCity/SpaceshipTests.java
+++ ExamPreparation/Exam19Dec2020/viceCity/SpaceshipTests.java
@@ -1,5 +1,4 @@
 public class SpaceshipTests {
-    @Test
     public void testShouldGetTheSpaceshipNameCorrectly() {
         String actualSpaceshipName = spaceship.getName();
         assertEquals(SPACESHIP_NAME, actualSpaceshipName);

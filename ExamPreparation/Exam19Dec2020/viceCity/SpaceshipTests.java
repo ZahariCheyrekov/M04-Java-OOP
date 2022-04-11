7389c8abf071c1197051637f7a54c8683478b669
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-04-11 22:05:18 +0300

    Update SpaceshipTests.java

--- ExamPreparation/Exam19Dec2020/viceCity/SpaceshipTests.java
+++ ExamPreparation/Exam19Dec2020/viceCity/SpaceshipTests.java
@@ -1,7 +1,4 @@
 public class SpaceshipTests {
-    public void testShouldThrowExceptionForTryingToSetInvalidSpaceshipCapacity() {
-    }
-
     @Test(expected = NullPointerException.class)
     public void testShouldThrowExceptionForTryingToSetNullNameToTheSpaceship() {
         new Spaceship(NULL_SPACESHIP_NAME, SPACESHIP_CAPACITY);

eca37e0a08637a36d188e2ca5dd3c8fda52bc4c9
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-04-12 23:00:07 +0300

    Update GarageTests.java

--- ExamPreparation/Exam20December2021/christmasRaces/GarageTests.java
+++ ExamPreparation/Exam20December2021/christmasRaces/GarageTests.java
@@ -13,6 +13,7 @@ public class GarageTests {
     private Car car;
 
     private static final String CAR_BRAND = "Lamborghini";
+    
     private static final int MAX_SPEED_LAMBORGHINI = 350;
     private static final double PRICE_LAMBORGHINI = 100_000;
 

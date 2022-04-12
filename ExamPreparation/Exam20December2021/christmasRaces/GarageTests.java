2137e157e0ef9370ab90921312a3a926301ee8a1
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-04-12 22:59:26 +0300

    Update GarageTests.java

--- ExamPreparation/Exam20December2021/christmasRaces/GarageTests.java
+++ ExamPreparation/Exam20December2021/christmasRaces/GarageTests.java
@@ -1,5 +1,4 @@
 public class GarageTests {
-    @Test
     public void testShouldFindAllCarsFromTheGarageWithMaxSpeedAboveGivenNumber() {
         List<Car> allCarsWithMaxSpeedAbove = garage.findAllCarsWithMaxSpeedAbove(CAR_SPEED);
         assertEquals(car, allCarsWithMaxSpeedAbove.get(CAR_LIST_INDEX));

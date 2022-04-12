238c267bb0deff9dc2f1acf177d752e5882cac88
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-04-12 22:59:17 +0300

    Update GarageTests.java

--- ExamPreparation/Exam20December2021/christmasRaces/GarageTests.java
+++ ExamPreparation/Exam20December2021/christmasRaces/GarageTests.java
@@ -1,7 +1,4 @@
 public class GarageTests {
-    public void testShouldFindAllCarsByTheGivenCarBrand() {
-    }
-
     @Test
     public void testShouldFindAllCarsFromTheGarageWithMaxSpeedAboveGivenNumber() {
         List<Car> allCarsWithMaxSpeedAbove = garage.findAllCarsWithMaxSpeedAbove(CAR_SPEED);

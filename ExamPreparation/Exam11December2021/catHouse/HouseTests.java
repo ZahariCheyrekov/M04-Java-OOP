b433dde7ea24aa09b33d65dba8d11af442db6607
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-04-13 20:48:07 +0300

    Update HouseTests.java

--- ExamPreparation/Exam11December2021/catHouse/HouseTests.java
+++ ExamPreparation/Exam11December2021/catHouse/HouseTests.java
@@ -1,30 +1,4 @@
 
 public class HouseTests {
-    @Test
-    public void testShouldRemoveCatCorrectlyFromTheCatHouse() {
-        house.removeCat(cat.getName());
-        assertEquals(ZERO_COUNT, house.getCount());
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testShouldThrowExceptionForNoSuchCatWithGivenName() {
-        house.removeCat(NONE_EXISTING_CAT);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testShouldThrowExceptionForNoSuchCatForSale() {
-        house.catForSale(NONE_EXISTING_CAT);
-    }
-
-    @Test
-    public void testShouldSetCatForSaleCorrectly() {
-        Cat catForSale = house.catForSale(cat.getName());
-        boolean isHungry = catForSale.isHungry();
-        assertFalse(isHungry);
-    }
-
-    @Test
-    public void testShouldReturnStatisticsForCatsInTheHouse() {
-        assertEquals(STATISTICS_MESSAGE, house.statistics());
-    }
+   
 }

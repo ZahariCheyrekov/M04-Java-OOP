67b2a7a2d1bcc57e0b092f0a5e518ab8b9ffe4f6
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-04-18 13:56:30 +0300

    Update ControllerImpl.java

--- ExamPreparation/Exam18April2022/zoo/core/ControllerImpl.java
+++ ExamPreparation/Exam18April2022/zoo/core/ControllerImpl.java
@@ -5,7 +5,6 @@ public class ControllerImpl implements Controller {
 
     @Override
     public String addArea(String areaType, String areaName) {
-        Area area;
 
         switch (areaType) {
             case "WaterArea":

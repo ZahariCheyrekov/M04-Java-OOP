ce066b7ac1d9c84bfc79c5da866267dad6cba639
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-04-19 10:24:39 +0300

    Update FarmvilleTests.java

--- ExamPreparation/Exam22August2021/glacialExpedition/FarmvilleTests.java
+++ ExamPreparation/Exam22August2021/glacialExpedition/FarmvilleTests.java
@@ -1,5 +1,4 @@
 public class FarmvilleTests {
-
   private static final String FARM_NAME = "HappyFarm";
   private static final String NULL_NAME = null;
   private static final int FARM_CAPACITY = 2;

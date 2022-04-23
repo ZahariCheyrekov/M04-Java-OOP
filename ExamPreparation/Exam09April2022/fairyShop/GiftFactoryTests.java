2693936bc94dd9e45fd293ffb5ed13a33efaa3cd
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-04-23 18:53:25 +0300

    Update GiftFactoryTests.java

--- ExamPreparation/Exam09April2022/fairyShop/GiftFactoryTests.java
+++ ExamPreparation/Exam09April2022/fairyShop/GiftFactoryTests.java
@@ -19,7 +19,7 @@ public class GiftFactoryTests {
     private static final String GIFT_BALL = "Ball";
     private static final double GIFT_MAGIC_TWO = 200;
 
-    private static final int ZERO_COUNT_GIFTS = 0; 
+    private static final int ZERO_COUNT_GIFTS = 0;  
     private static final int GIFTS_COUNT = 1; 
     private static final int GIFTS_COUNT_TWO = 2;
 
